7e39ae51277b00d441d79ace67601bf5606ab5d2
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-04-21 15:56:19 +0300

    Arrays - exercise, 2 problems

--- Technology-Fundamentals/Arrays/09.Kamino-Factory/Program.cs
+++ Technology-Fundamentals/Arrays/09.Kamino-Factory/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _09.Kamino_Factory
 {
@@ -6,7 +7,81 @@ namespace _09.Kamino_Factory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int length = int.Parse(Console.ReadLine());
+            string command = string.Empty;
+
+            int bestSubsequenceLength = -1;
+            int bestSubsequenceIndex = -1;
+            int bestSubsequenceSum = 0;
+
+            int[] bestSequence = new int[length];
+
+            int counter = 0;
+            int bestCounter = 0;
+
+            while ((command = Console.ReadLine()) != "Clone them!")
+            {
+                int[] sequence = command
+                    .Split('!', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                counter++;
+
+                int subsequenceMaxLength = 0;
+                int subsequenceMaxIndex = -1;
+                int subsequenceMaxSum = 0;
+                int subsequenceLength = 0;
+
+
+                for (int i = 0; i < sequence.Length; i++)
+                {
+                    if (sequence[i] == 1)
+                    {
+                        subsequenceLength++;
+                        subsequenceMaxSum++;   
+
+                        if (subsequenceLength > subsequenceMaxLength)
+                        {
+                            subsequenceMaxIndex = i - subsequenceLength;
+                            subsequenceMaxLength = subsequenceLength;
+                        }
+                    }
+                    else
+                    {
+                        subsequenceLength = 0;
+                    }
+
+                }
+
+                if (subsequenceMaxLength > bestSubsequenceLength)
+                {
+                    bestSubsequenceLength = subsequenceMaxLength;
+                    bestSubsequenceIndex = subsequenceMaxIndex;
+                    bestSubsequenceSum = subsequenceMaxSum;
+                    bestSequence = sequence;
+                    bestCounter = counter;
+                }
+                else if (subsequenceMaxLength == bestSubsequenceLength 
+                    && subsequenceMaxIndex < bestSubsequenceIndex)
+                {
+                    bestSubsequenceIndex = subsequenceMaxIndex;
+                    bestSubsequenceSum = subsequenceMaxSum;
+                    bestSequence = sequence;
+                    bestCounter = counter;
+                }
+                else if (subsequenceMaxLength == bestSubsequenceLength
+                    && subsequenceMaxIndex == bestSubsequenceIndex
+                    && subsequenceMaxSum > bestSubsequenceSum)
+                {                    
+                    bestSubsequenceSum = subsequenceMaxSum;
+                    bestSequence = sequence;
+                    bestCounter = counter;
+                }
+            }
+
+            Console.WriteLine($"Best DNA sample {bestCounter} with sum: {bestSubsequenceSum}.");
+            Console.WriteLine(string.Join(' ', bestSequence));
         }
     }
 }
---
+++ Technology-Fundamentals/Arrays/10.Lady-Bugs/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.Lady_Bugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] initialIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] field = new int[size];
+
+            for (int i = 0; i < initialIndexes.Length; i++)
+            {
+                if (initialIndexes[i] >= 0 && initialIndexes[i] < size)
+                {
+                    field[initialIndexes[i]] = 1;
+                }
+            }
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] tokens = command.Split();
+                int startIndex = int.Parse(tokens[0]);
+                int flyLength = int.Parse(tokens[2]);
+                string direction = tokens[1];
+
+
+                if ((startIndex < 0 || startIndex >= size) || 
+                    flyLength == 0 || 
+                    field[startIndex] == 0)
+                {
+                    continue;
+                }
+
+                int endIndex = 0;
+
+                if (direction == "right")
+                {
+                    endIndex = startIndex + flyLength;
+                }
+                else if (direction == "left")
+                {
+                    endIndex = startIndex - flyLength;
+                }
+
+                if (endIndex < 0 || endIndex >= size)
+                {
+                    field[startIndex] = 0;
+                    continue;
+                }
+
+                if (field[endIndex] == 0)
+                {
+                    field[startIndex] = 0;
+                    field[endIndex] = 1;                    
+                }
+                else
+                {
+                    while (true)
+                    {
+                        if (direction == "right")
+                        {
+                            endIndex = endIndex + flyLength;
+                        }
+                        else if (direction == "left")
+                        {
+                            endIndex = endIndex - flyLength;
+                        }
+
+                        if (endIndex < 0 || endIndex >= size)
+                        {                            
+                            break;
+                        }
+
+                        if (field[endIndex] == 0)
+                        {
+                            field[endIndex] = 1;
+                            break;
+                        }
+                    }
+
+                    field[startIndex] = 0;
+                }
+
+            }
+
+            Console.WriteLine(string.Join(' ', field));
+
+        }
+    }
+}
