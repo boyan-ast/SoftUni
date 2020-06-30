d8b0f33b3ebbd8bc2896411c03f388ae26d0ee29
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-06-30 15:57:06 +0300

    Objects and classes - Order By Age problem

--- Technology-Fundamentals/ObjectsAndClasses/01.AdvertisementMesssage/Program.cs
+++ Technology-Fundamentals/ObjectsAndClasses/01.AdvertisementMesssage/Program.cs
@@ -6,7 +6,51 @@ namespace _01.AdvertisementMesssage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] phrases = new string[]
+                {   "Excellent product.",
+                    "Such a great product.",
+                    "I always use that product.",
+                    "Best product of its category.",
+                    "Exceptional product.",
+                    "I can’t live without this product." };
+
+            string[] events = new string[]
+            {   "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!" };
+
+            string[] authors = new string[]
+                {   "Diana",
+                    "Petya",
+                    "Stella",
+                    "Elena",
+                    "Katya",
+                    "Iva",
+                    "Annie",
+                    "Eva" };
+            string[] cities = new string[]
+                {   "Burgas",
+                    "Sofia",
+                    "Plovdiv",
+                    "Varna",
+                    "Ruse" };
+
+            int messages = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < messages; i++)
+            {
+                Random rnd = new Random();
+
+                int phraseIndex = rnd.Next(phrases.Length);
+                int eventIndex = rnd.Next(events.Length);
+                int authorIndex = rnd.Next(authors.Length);
+                int cityIndex = rnd.Next(cities.Length);
+
+                Console.WriteLine($"{phrases[phraseIndex]} {events[eventIndex]} {authors[authorIndex]} – {cities[cityIndex]}");
+            }
         }
     }
 }
--- Technology-Fundamentals/ObjectsAndClasses/07.OrderByAge/Program.cs
+++ Technology-Fundamentals/ObjectsAndClasses/07.OrderByAge/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _07.OrderByAge
 {
@@ -6,50 +8,43 @@ namespace _07.OrderByAge
     {
         static void Main(string[] args)
         {
-            string[] phrases = new string[]
-                {   "Excellent product.",
-                    "Such a great product.",
-                    "I always use that product.",
-                    "Best product of its category.",
-                    "Exceptional product.",
-                    "I can’t live without this product." };
+            string input = Console.ReadLine();
+            List<UserInfo> users = new List<UserInfo>();
 
-            string[] events = new string[]
-            {   "Now I feel good.",
-                "I have succeeded with this product.",
-                "Makes miracles. I am happy of the results!",
-                "I cannot believe but now I feel awesome.",
-                "Try it yourself, I am very satisfied.",
-                "I feel great!" };
-
-            string[] authors = new string[]
-                {   "Diana",
-                    "Petya",
-                    "Stella",
-                    "Elena",
-                    "Katya",
-                    "Iva",
-                    "Annie",
-                    "Eva" };
-            string[] cities = new string[]
-                {   "Burgas",
-                    "Sofia",
-                    "Plovdiv",
-                    "Varna",
-                    "Ruse" };
-
-            int messages = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < messages; i++)
+            while (input != "End")
             {
-                Random rnd = new Random();
+                string[] personData = input.Split();
+                string name = personData[0];
+                string id = personData[1];
+                int age = int.Parse(personData[2]);
 
-                int phraseIndex = rnd.Next(phrases.Length);
-                int eventIndex = rnd.Next(events.Length);
-                int authorIndex = rnd.Next(authors.Length);
-                int cityIndex = rnd.Next(cities.Length);
+                UserInfo user = new UserInfo(name, id, age);
+                users.Add(user);
 
-                Console.WriteLine($"{phrases[phraseIndex]} {events[eventIndex]} {authors[authorIndex]} – {cities[cityIndex]}");
+                input = Console.ReadLine();
+            }
+
+            foreach (UserInfo user in users.OrderBy(x => x.Age))
+            {
+                Console.WriteLine(user);
+            }
+        }
+        class UserInfo
+        {
+            public string Name { get; set; }
+            public string ID { get; set; }
+            public int Age { get; set; }
+
+            public UserInfo(string name, string id, int age)
+            {
+                Name = name;
+                ID = id;
+                Age = age;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name} with ID: {ID} is {Age} years old.";
             }
         }
     }
