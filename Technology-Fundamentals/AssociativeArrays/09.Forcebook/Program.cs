ca8adc5de094ba5ff82d87db2cc641fb86ccdb48
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-07-08 15:56:35 +0300

    Associative Arrays - exam problem SoftUniExamResults

---
+++ Technology-Fundamentals/AssociativeArrays/09.Forcebook/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.Forcebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> forceBook = new Dictionary<string, List<string>>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Lumpawaroo")
+            {
+                string[] inputInfo = new string[2];
+                string forceUser = string.Empty;
+                string forceSide = string.Empty;
+
+                bool forceUserExists = false;
+
+                if (command.Split(" | ").Length == 2)
+                {
+                    inputInfo = command.Split(" | ");
+                    forceSide = inputInfo[0];
+                    forceUser = inputInfo[1];
+
+                    foreach (var kvp in forceBook)
+                    {
+                        if (kvp.Value.Contains(forceUser))
+                        {
+                            forceUserExists = true;
+                            break;
+                        }
+                    }
+
+                    if (!forceUserExists)
+                    {
+                        if (!forceBook.ContainsKey(forceSide))
+                        {
+                            forceBook[forceSide] = new List<string>();
+                        }
+
+                        forceBook[forceSide].Add(forceUser);
+                    }
+                }
+                else if (command.Split(" -> ").Length == 2)
+                {
+                    inputInfo = command.Split(" -> ");
+                    forceSide = inputInfo[1];
+                    forceUser = inputInfo[0];
+
+                    foreach (var kvp in forceBook)
+                    {
+                        if (kvp.Value.Contains(forceUser))
+                        {
+                            forceUserExists = true;
+                            kvp.Value.Remove(forceUser);
+                            break;
+                        }
+                    }
+
+                    if (!forceBook.ContainsKey(forceSide))
+                    {
+                        forceBook[forceSide] = new List<string>();
+                    }
+
+                    forceBook[forceSide].Add(forceUser);
+
+                    Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                }
+            }
+
+            foreach (var kvp in forceBook.OrderByDescending(x => x.Value.Count).ThenBy(y => y.Key))
+            {
+                if (kvp.Value.Count > 0)
+                {
+                    Console.WriteLine($"Side: {kvp.Key}, Members: {kvp.Value.Count}");
+
+                    foreach (string user in kvp.Value.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"! {user}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Technology-Fundamentals/AssociativeArrays/10.SoftUniExamResults/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.SoftUniExamResults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            Dictionary<string, int> examSubmissions = new Dictionary<string, int>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "exam finished")
+            {
+                string[] userInfo = command.Split("-");
+                string username = userInfo[0];
+                int points = 0;
+
+                if (userInfo.Length == 3)
+                {
+                    string language = userInfo[1];
+                    points = int.Parse(userInfo[2]);
+
+                    if (!examSubmissions.ContainsKey(language))
+                    {
+                        examSubmissions[language] = 0;
+                    }
+                    examSubmissions[language]++;
+                }
+                else if (userInfo.Length == 2 && users.Select(x => x.Name).Contains(username))
+                {
+                    User existingUser = users.First(x => x.Name == username);
+                    users.Remove(existingUser);
+                    continue;
+                }
+
+                User user = new User();
+                user.Name = username;
+
+                if (!users.Select(x => x.Name).Contains(username))
+                {
+                    user.Points = points;
+                    users.Add(user);
+                }
+                else
+                {
+                    User existingUser = users.First(x => x.Name == username);
+
+                    if (existingUser.Points < points)
+                    {
+                        existingUser.Points = points;
+                    }
+                }
+            }
+
+            Console.WriteLine("Results:");
+            foreach (User username in users.OrderByDescending(x => x.Points).ThenBy(y => y.Name))
+            {
+                Console.WriteLine(username);
+            }
+            Console.WriteLine("Submissions:");
+            foreach (var kvp in examSubmissions.OrderByDescending(x => x.Value).ThenBy(y => y.Key))
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+        }
+        class User
+        {
+            public string Name { get; set; }
+            public int Points { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Name} | {Points}";
+            }
+        }
+    }
+}
