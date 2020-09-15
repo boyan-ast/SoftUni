b57ba784c6ce3e055b4a22f515887e42b717d4b9
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-09-15 13:43:36 +0300

    C# Fundamentals, Intro and Basic Syntax Lab

---
+++ C# Fundamentals/IntroAndBasicSyntax/04.Backin30Minutes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04.Backin30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int timeInMinutes = hours * 60 + minutes + 30;
+            int finalHour = timeInMinutes / 60;
+
+            if (finalHour == 24)
+            {
+                finalHour = 0;
+            }
+
+            int finalMinutes = timeInMinutes % 60;
+
+            Console.WriteLine($"{finalHour}:{finalMinutes:d2}");
+        }
+    }
+}
---
+++ C# Fundamentals/IntroAndBasicSyntax/09.SumOfOddNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _09.SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int number = 1;
+            int sum = 0;
+
+            for (int i = 0; i < numbers; i++)
+            {
+                Console.WriteLine(number);
+                sum += number;
+                number += 2;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
