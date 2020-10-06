89c9171c9e074e6116b38e4ddc56c53dd9ae649d
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-10-06 14:40:25 +0300

    Methods - More exercise, last problem

--- C# Fundamentals/Methods/05.MultiplicationSign/Program.cs
+++ C# Fundamentals/Methods/05.MultiplicationSign/Program.cs
@@ -6,7 +6,43 @@ namespace _05.MultiplicationSign
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstNum = Console.ReadLine();
+            string secondNum = Console.ReadLine();
+            string thirdNum = Console.ReadLine();
+
+            FindResultSign(firstNum, secondNum, thirdNum);
+        }
+
+        private static void FindResultSign(string a, string b, string c)
+        {           
+
+            if (a == "0" || b == "0" || c == "0")
+            {
+                Console.WriteLine("zero");
+            }
+            else
+            {
+                int minuses = 0;
+
+                string[] nums = new string[] { a, b, c };
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i][0] == '-')
+                    {
+                        minuses++;
+                    }
+                }
+
+                if (minuses == 1 || minuses == 3)
+                {
+                    Console.WriteLine("negative");
+                }
+                else
+                {
+                    Console.WriteLine("positive");
+                }
+            }
         }
     }
 }
