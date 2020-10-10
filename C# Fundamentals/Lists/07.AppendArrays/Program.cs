b99140ff1a344a68c64ad1f7a1feb9c076a804af
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-10-10 14:47:07 +0300

    Lists - Exercise, Append Arrays problem

---
+++ C# Fundamentals/Lists/07.AppendArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _07.AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> allArraysToString = Console.ReadLine()
+                .Split("|")
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToList();
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = allArraysToString.Count - 1; i >= 0; i--)
+            {
+                int[] currentArray = allArraysToString[i]
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                result.Append(string.Join(" ", currentArray));
+                result.Append(" ");
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
