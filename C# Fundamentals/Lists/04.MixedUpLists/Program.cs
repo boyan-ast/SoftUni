79afec6dd2ae0871e7109845b3b87986fa260c2f
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-10-14 10:48:07 +0300

    Lists - More exercise, last two problems solved

---
+++ C# Fundamentals/Lists/04.MixedUpLists/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.MixedUpLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> secondList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> mixedList = new List<int>();
+
+            for (int i = 0; i < Math.Min(firstList.Count, secondList.Count); i++)
+            {
+                mixedList.Add(firstList[i]);
+                mixedList.Add(secondList[secondList.Count - 1 - i]);
+            }
+
+            List<int> constrains = new List<int>();
+
+            if (firstList.Count > secondList.Count)
+            {
+                constrains.Add(firstList[firstList.Count - 1]);
+                constrains.Add(firstList[firstList.Count - 2]);
+            }
+            else
+            {
+                constrains.Add(secondList[0]);
+                constrains.Add(secondList[1]);
+            }
+
+            constrains.Sort();
+
+            int min = constrains[0];
+            int max = constrains[1];
+
+            List<int> result = mixedList
+                .Where(x => (x > min && x < max))
+                .OrderBy(n => n)
+                .ToList();
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ C# Fundamentals/Lists/05.DrumSet/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.DrumSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+
+            List<int> powers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToList();
+
+            List<int> initialPowers = new List<int>(powers);
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                int hitPower = int.Parse(command);
+
+                for (int i = 0; i < powers.Count; i++)
+                {
+                    powers[i] -= hitPower;
+
+                    if (powers[i] <= 0)
+                    {
+                        double drumPrice = initialPowers[i] * 3;
+
+                        if ((budget - drumPrice) < 0)
+                        {
+                            powers.RemoveAt(i);
+                            initialPowers.RemoveAt(i);
+                            i--;
+                        }
+                        else
+                        {
+                            budget -= drumPrice;
+                            powers[i] = initialPowers[i];
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", powers));
+            Console.WriteLine($"Gabsy has {budget:f2}lv.");
+        }
+    }
+}
