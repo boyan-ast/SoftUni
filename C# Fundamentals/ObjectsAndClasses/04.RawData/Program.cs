03b69c4c232d44534513d6e2646cc89e20878cd4
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-10-25 12:41:01 +0200

    Objects and Classes, More exercise - 2 problems solved

---
+++ C# Fundamentals/ObjectsAndClasses/04.RawData/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.RawData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            List<Car> allCars = new List<Car>(numberOfCars);
+
+            AddCars(numberOfCars, allCars);
+
+            string type = Console.ReadLine();
+            List<Car> filteredCars = FilterTheCars(allCars, type);
+
+            foreach (Car car in filteredCars)
+            {
+                Console.WriteLine(car);
+            }
+            
+        }
+
+        private static List<Car> FilterTheCars(List<Car> allCars, string type)
+        {
+            if (type == "fragile")
+            {
+                return allCars.Where(x =>(x.CarCargo.Type == type && x.CarCargo.Weight < 1000)).ToList();
+            }
+            else if (type == "flamable")
+            {
+                return allCars.Where(x => (x.CarCargo.Type == type && x.CarEngine.Power > 250)).ToList();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private static void AddCars(int numberOfCars, List<Car> allCars)
+        {
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] carData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = carData[0];
+                int speed = int.Parse(carData[1]);
+                int power = int.Parse(carData[2]);
+                int cargoWeight = int.Parse(carData[3]);
+                string cargoType = carData[4];
+
+                Car car = new Car(model, cargoWeight, cargoType, speed, power);
+
+                allCars.Add(car);
+            }
+        }
+    }
+
+    class Car
+    {
+        public string Model { get; set; }
+        public Engine CarEngine { get; set; }
+        public Cargo CarCargo { get; set; }
+
+        public Car(string model, int cargoWeight, string cargoType, int engineSpeed, int enginePower)
+        {
+            Model = model;
+            CarEngine = new Engine(engineSpeed, enginePower);
+            CarCargo = new Cargo(cargoWeight, cargoType);
+        }
+
+        public override string ToString()
+        {
+            return Model;
+        }
+    }
+
+    class Cargo
+    {
+        public int Weight { get; set; }
+        public string Type { get; set; }
+
+        public Cargo(int weight, string type)
+        {
+            Weight = weight;
+            Type = type;
+        }
+    }
+
+    class Engine
+    {
+        public int Speed { get; set; }
+        public int Power { get; set; }
+
+        public Engine(int speed, int power)
+        {
+            Speed = speed;
+            Power = power;
+        }
+    }
+}
---
+++ C# Fundamentals/ObjectsAndClasses/05.ShoppingSpree/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.ShoppingSpree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] peopleData = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+            List<Person> people = new List<Person>(peopleData.Length);
+
+            AddBuyers(peopleData, people);
+
+            string[] productsData = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+            List<Product> products = new List<Product>(productsData.Length);
+
+            AddProducts(productsData, products);
+
+            BuyProducts(people, products);
+
+            PrintProductBags(people);
+        }
+
+        private static void PrintProductBags(List<Person> people)
+        {
+            foreach (Person person in people)
+            {
+                if (person.BagOfProducts.Count > 0)
+                {
+                    Console.WriteLine($"{person.Name} - " + string.Join(", ", person.BagOfProducts));
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Name} - Nothing bought");
+                }
+            }
+        }
+
+        static void BuyProducts(List<Person> people, List<Product> products)
+        {
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] commandArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string person = commandArgs[0];
+                string item = commandArgs[1];
+
+                Person buyer = people.First(x => x.Name == person);
+                Product product = products.First(x => x.Name == item);
+
+                buyer.BuyProduct(product);
+            }
+        }
+        private static void AddProducts(string[] productsData, List<Product> products)
+        {
+            foreach (string item in productsData)
+            {
+                string[] itemData = item.Split('=', StringSplitOptions.RemoveEmptyEntries);
+                string name = itemData[0];
+                int cost = int.Parse(itemData[1]);
+
+                Product product = new Product(name, cost);
+                products.Add(product);
+            }
+        }
+
+        private static void AddBuyers(string[] peopleData, List<Person> people)
+        {
+            foreach (string buyer in peopleData)
+            {
+                string[] personData = buyer.Split('=', StringSplitOptions.RemoveEmptyEntries);
+                string name = personData[0];
+                int money = int.Parse(personData[1]);
+
+                Person person = new Person(name, money);
+                people.Add(person);
+            }
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Money { get; set; }
+        public List<Product> BagOfProducts { get; set; }
+
+        public Person(string name, int money)
+        {
+            Name = name;
+            Money = money;
+            BagOfProducts = new List<Product>();
+        }
+
+        public void BuyProduct(Product product)
+        {
+            if (product.Cost <= Money)
+            {
+                Console.WriteLine($"{Name} bought {product.Name}");
+                BagOfProducts.Add(product);
+                Money -= product.Cost;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} can't afford {product.Name}");
+            }
+        }
+    }
+
+    class Product
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+
+        public Product(string name, int cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
