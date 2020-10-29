9a9e228676ff8cf4bd9b19479f262ce6178fcc30
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-10-29 16:17:57 +0200

    Associative Arrays - Exercise problems

---
+++ C# Fundamentals/AssociativeArrays/06.Courses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] studentInfo = command.Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+
+                string courseName = studentInfo[0];
+                string studentName = studentInfo[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses[courseName] = new List<string>();
+                }
+
+                courses[courseName].Add(studentName);
+            }
+
+            foreach (var kvp in courses.OrderByDescending(x => x.Value.Count))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count}");
+
+                foreach (string student in kvp.Value.OrderBy(s => s))
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/AssociativeArrays/07.StudentAcademy/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> allStudents = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                Student student = allStudents.FirstOrDefault(x => x.Name == name);
+
+                if (student == null)
+                {
+                    student = new Student(name);
+                    allStudents.Add(student);
+                }
+
+                student.AddGrade(grade);
+            }
+
+            List<Student> filteredStudents = new List<Student>();
+
+            foreach (Student student in allStudents)
+            {
+                if (student.AverageGrade() >= 4.50)
+                {
+                    filteredStudents.Add(student);
+                }
+            }
+
+            filteredStudents = filteredStudents.OrderByDescending(x => x.AverageGrade()).ToList();
+
+            foreach (Student student in filteredStudents)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public List<double> Grades { get; set; }
+
+        private double averageGrade;
+
+        public Student(string name)
+        {
+            Name = name;
+            Grades = new List<double>();
+        }
+
+        public void AddGrade(double grade)
+        {
+            Grades.Add(grade);
+        }
+
+        public double AverageGrade()
+        {
+            averageGrade = Grades.Average();
+            return averageGrade;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} -> {averageGrade:f2}";             
+        }
+    }
+}
