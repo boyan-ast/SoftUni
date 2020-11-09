9de2747b5f2e99786717e2d395e679baf610c3ec
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-11-09 14:08:27 +0200

    Text Processing - Exercise problems

---
+++ C# Fundamentals/StringsAndTextProcessing/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _06.ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder resultText = new StringBuilder();
+            resultText.Append(input[0]);
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] != input[i-1])
+                {
+                    resultText.Append(input[i]);
+                }
+            }
+
+            Console.WriteLine(resultText);
+        }
+    }
+}
---
+++ C# Fundamentals/StringsAndTextProcessing/07.StringExplosion/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace _07.StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+
+            for (int index = 0; index < text.Length; index++)
+            {
+                result.Append(text[index]);
+
+                if (text[index] == '>')
+                {
+                    index++;
+                    int count = int.Parse(text[index].ToString());
+
+                    while (count > 0)
+                    {
+                        index++;
+                        count--;
+
+                        if (index == text.Length)
+                        {
+                            break;
+                        }
+
+                        if (text[index] == '>')
+                        {
+                            result.Append(text[index]);
+                            index++;
+
+                            if (index == text.Length)
+                            {
+                                break;
+                            }
+
+                            count += int.Parse(text[index].ToString());
+                        }
+                    }
+
+                    index--;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
