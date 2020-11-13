6a2af77812336606581e73897be0ee024f87e8d7
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-11-13 11:33:06 +0200

    Regular Expressions - Exercise problems

---
+++ C# Fundamentals/RegularExpressions/04.StarEnigma/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _04.StarEnigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> attackedPlanets = new List<string>();
+            List<string> destroyedPlanets = new List<string>();
+            string pattern = @"@(?<planet>[A-Z][a-z]*)[^@\-!:>]*:(?<population>\d+)[^@\-!:>]*!(?<attackType>[AD])![^@\-!:>]*->(?<soldiers>\d+)";
+
+            for (int i = 0; i < n; i++)
+            {
+                string message = Console.ReadLine();
+
+                int key = CalculateKey(message.ToLower());
+
+                string decryptedMessage = DecryptMessageWithKey(message, key);
+
+                MatchCollection info = Regex.Matches(decryptedMessage, pattern);
+
+                foreach (Match match in info)
+                {
+                    string planetName = match.Groups["planet"].Value;
+                    char attackType = char.Parse(match.Groups["attackType"].Value);
+
+                    if (attackType == 'A')
+                    {
+                        attackedPlanets.Add(planetName);
+                    }
+                    else if (attackType == 'D')
+                    {
+                        destroyedPlanets.Add(planetName);
+                    }
+                }
+
+            }
+
+            Console.WriteLine($"Attacked planets: {attackedPlanets.Count}");
+            foreach (string planet in attackedPlanets.OrderBy(x => x))
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+
+            Console.WriteLine($"Destroyed planets: {destroyedPlanets.Count}");
+            foreach (string planet in destroyedPlanets.OrderBy(x => x))
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+        }
+
+        static string DecryptMessageWithKey(string message, int key)
+        {
+            StringBuilder result = new StringBuilder(message.Length);
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                result.Append((char)(message[i] - key));
+            }
+
+            return result.ToString();
+        }
+
+        static int CalculateKey(string text)
+        {
+            int count = 0;
+
+            foreach (char symbol in text)
+            {
+                if (symbol == 's' || symbol == 't' || symbol == 'a' || symbol == 'r')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ C# Fundamentals/RegularExpressions/05.NetherRealms/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _05.NetherRealms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] demonsInfo = Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            List<Demon> allDemons = new List<Demon>(demonsInfo.Length);
+
+            string healthPattern = @"[^0-9\+\-\*\/\.]";
+            string numsPattern = @"[\+\-]?\d+(\.\d+)?";
+
+            foreach (string name in demonsInfo)
+            {
+                var chars = Regex.Matches(name, healthPattern);
+                int health = 0;
+
+                foreach (Match match in chars)
+                {
+                    health += char.Parse(match.Value);
+                }
+
+                var nums = Regex.Matches(name, numsPattern);
+                double damage = 0;
+
+                foreach (Match match in nums)
+                {
+                    damage += double.Parse(match.Value);
+                }
+
+                for (int i = 0; i < name.Length; i++)
+                {
+                    if (name[i]=='*')
+                    {
+                        damage *= 2;
+                    }
+                    else if (name[i] == '/')
+                    {
+                        damage /= 2.00;
+                    }
+                }
+
+                Demon demon = new Demon(name, health, damage);
+                allDemons.Add(demon);
+            }
+
+            foreach (Demon demon in allDemons.OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{demon.Name} - {demon.Health} health, {demon.Damage:f2} damage");
+            }
+
+        }
+    }
+
+    class Demon
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public double Damage { get; set; }
+
+        public Demon(string name, int health, double damage)
+        {
+            Name = name;
+            Health = health;
+            Damage = damage;
+        }
+    }
+}
