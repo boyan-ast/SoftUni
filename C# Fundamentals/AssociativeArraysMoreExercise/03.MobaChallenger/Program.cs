8bee7b0a10264d0af396d9bee98a0fb0b8b9d748
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-11-16 12:03:42 +0200

    Associative Arrays - More Exercise, 2 problems

---
+++ C# Fundamentals/AssociativeArraysMoreExercise/03.MobaChallenger/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MobaChallenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var players = new Dictionary<string, Dictionary<string, int>>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Season end")
+            {
+                string[] commandArgs = command
+                    .Split(new string[] { " -> ", " vs " }, StringSplitOptions.RemoveEmptyEntries);
+
+                string player = commandArgs[0];
+
+                if (commandArgs.Length == 3)
+                {
+                    string position = commandArgs[1];
+                    int skill = int.Parse(commandArgs[2]);
+
+                    if (players.ContainsKey(player))
+                    {
+                        if (players[player].ContainsKey(position))
+                        {
+                            if (players[player][position] < skill)
+                            {
+                                players[player][position] = skill;
+                            }
+                        }
+                        else
+                        {
+                            players[player].Add(position, skill);
+                        }
+                    }
+                    else
+                    {
+                        players.Add(player, new Dictionary<string, int>());
+                        players[player].Add(position, skill);
+                    }
+                }
+                else if (commandArgs.Length == 2)
+                {
+                    string secondPlayer = commandArgs[1];
+
+                    if (players.ContainsKey(player) && players.ContainsKey(secondPlayer))
+                    {
+                        bool hasCommonPosition = false;
+
+                        foreach (var kvp in players[player])
+                        {
+                            string firstPlayerPos = kvp.Key;
+
+                            foreach (var secondKvp in players[secondPlayer])
+                            {
+                                string secondPlayerPos = secondKvp.Key;
+
+                                if (firstPlayerPos == secondPlayerPos)
+                                {
+                                    hasCommonPosition = true;
+                                    break;
+                                }
+                            }
+
+                            if (hasCommonPosition)
+                            {
+                                break;
+                            }
+                        }
+
+                        if (hasCommonPosition)
+                        {
+                            int firstPlayerTotalSkill = players[player].Sum(x => x.Value);
+                            int secondPlayerTotalSkill = players[secondPlayer].Sum(x => x.Value);
+
+                            if (firstPlayerTotalSkill > secondPlayerTotalSkill)
+                            {
+                                players.Remove(secondPlayer);
+                            }
+                            else if (firstPlayerTotalSkill < secondPlayerTotalSkill)
+                            {
+                                players.Remove(player);
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (var kvp in players.OrderByDescending(x => x.Value.Sum(y => y.Value)).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Sum(x => x.Value)} skill");
+
+                foreach (var item in kvp.Value.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"- {item.Key} <::> {item.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/AssociativeArraysMoreExercise/04.Snowwhite/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Snowwhite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dwarfs = new Dictionary<string, int>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Once upon a time")
+            {
+                string[] dwarfData = command.Split(" <:> ", StringSplitOptions.RemoveEmptyEntries);
+
+                string colorPlusName = dwarfData[1] + " " + dwarfData[0];
+                int physics = int.Parse(dwarfData[2]);
+
+                if (!dwarfs.ContainsKey(colorPlusName))
+                {
+                    dwarfs.Add(colorPlusName, physics);
+                }
+                else
+                {
+                    if (dwarfs[colorPlusName] < physics)
+                    {
+                        dwarfs[colorPlusName] = physics;
+                    }
+                }
+            }
+
+            List<Dwarf> allDwarfs = new List<Dwarf>(dwarfs.Count);
+
+            foreach (var kvp in dwarfs)
+            {
+                string color = kvp.Key.Split()[0];
+                string name = kvp.Key.Split()[1];
+                int physics = kvp.Value;
+
+                Dwarf dwarf = new Dwarf(color, name, physics);
+
+                if (allDwarfs.FirstOrDefault(x => x.Color == color) != null)
+                {
+                    foreach (Dwarf item in allDwarfs.Where(x => x.Color == color))
+                    {
+                        item.ColorCounter++;
+                    }
+
+                    dwarf.ColorCounter = allDwarfs.FirstOrDefault(x => x.Color == color).ColorCounter;
+                }
+
+                allDwarfs.Add(dwarf);
+            }
+
+
+            foreach (Dwarf dwarf in allDwarfs.OrderByDescending(x => x.Physics)
+                .ThenByDescending(x => x.ColorCounter))
+            {
+                Console.WriteLine(dwarf);
+            }
+
+        }
+    }
+
+    class Dwarf
+    {
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public int Physics { get; set; }
+        public int ColorCounter { get; set; }
+
+        public Dwarf(string color, string name, int physics)
+        {
+            Color = color;
+            Name = name;
+            Physics = physics;
+            ColorCounter = 1;
+        }
+
+        public override string ToString()
+        {
+            return $"({Color}) {Name} <-> {Physics}";
+        }
+    }
+}
