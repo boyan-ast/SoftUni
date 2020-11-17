3a2ea49dd3d38c4f47305f588490a13012a7667d
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-11-17 10:45:58 +0200

    Associative Arrays - More Exercise, last problem

---
+++ C# Fundamentals/AssociativeArraysMoreExercise/05.DragonArmy/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.DragonArmy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Dragon> allDragons = new List<Dragon>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] dragonInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string type = dragonInfo[0];
+                string name = dragonInfo[1];
+
+                int damage = 0;
+                if (!int.TryParse(dragonInfo[2], out damage))
+                {
+                    damage = 45;
+                }
+
+                int health = 0;
+                if (!int.TryParse(dragonInfo[3], out health))
+                {
+                    health = 250;
+                }
+
+                int armor = 0;
+                if (!int.TryParse(dragonInfo[4], out armor))
+                {
+                    armor = 10;
+                }
+
+
+                Dragon existingOne = allDragons.FirstOrDefault(x => (x.Type == type && x.Name == name));
+
+                if (existingOne != null)
+                {
+                    existingOne.Health = health;
+                    existingOne.Damage = damage;
+                    existingOne.Armor = armor;
+                }
+                else
+                {
+                    Dragon newOne = new Dragon(name, type, damage, health, armor);
+                    allDragons.Add(newOne);
+                }
+            }            
+
+            Dictionary<string, List<Dragon>> dragonsByType = new Dictionary<string, List<Dragon>>();
+
+            foreach (Dragon dragon in allDragons)
+            {
+                string type = dragon.Type;
+
+                if (!dragonsByType.ContainsKey(type))
+                {
+                    dragonsByType[type] = new List<Dragon>();
+                }
+
+                dragonsByType[type].Add(dragon);
+            }
+
+            foreach (var kvp in dragonsByType)
+            {
+                double averageDamage = kvp.Value.Average(x => x.Damage);
+                double averageHealth = kvp.Value.Average(x => x.Health);
+                double averageArmor = kvp.Value.Average(x => x.Armor);
+
+                Console.WriteLine($"{kvp.Key}::({averageDamage:f2}/{averageHealth:f2}/{averageArmor:f2})");
+
+                foreach (Dragon dragon in kvp.Value.OrderBy(x => x.Name))
+                {
+                    Console.WriteLine($"-{dragon.Name} -> damage: {dragon.Damage}, health: {dragon.Health}, armor: {dragon.Armor}");
+                }
+            }
+        }
+    }
+
+    class Dragon
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Damage { get; set; }
+        public int Health { get; set; }
+        public int Armor { get; set; }
+
+        public Dragon(string name, string type, int damage, int health, int armor)
+        {
+            Name = name;
+            Type = type;
+            Damage = damage;
+            Health = health;
+            Armor = armor;
+        }
+    }
+}
---
+++ C# Fundamentals/AssociativeArraysMoreExercise/05.DragonArmy_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.DragonArmy_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dragons = new Dictionary<string, SortedDictionary<string, int[]>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] dragonInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string type = dragonInfo[0];
+                string name = dragonInfo[1];
+
+                int damage = dragonInfo[2] == "null" ? 45 : int.Parse(dragonInfo[2]);
+                int health = dragonInfo[3] == "null" ? 250 : int.Parse(dragonInfo[3]);
+                int armor = dragonInfo[4] == "null" ? 10 : int.Parse(dragonInfo[4]);
+
+                if (!dragons.ContainsKey(type))
+                {
+                    dragons[type] = new SortedDictionary<string, int[]>();
+                }
+
+                if (!dragons[type].ContainsKey(name))
+                {
+                    dragons[type][name] = new int[3];
+                }
+
+                dragons[type][name][0] = damage;
+                dragons[type][name][1] = health;
+                dragons[type][name][2] = armor;
+            }
+
+            foreach (var globalKvp in dragons)
+            {
+                double averageDamage = globalKvp.Value.Sum(x => x.Value[0]) * 1.0 / globalKvp.Value.Count;
+                double averageHealth = globalKvp.Value.Sum(x => x.Value[1]) * 1.0 / globalKvp.Value.Count;
+                double averageArmor = globalKvp.Value.Sum(x => x.Value[2]) * 1.0 / globalKvp.Value.Count;
+
+                Console.WriteLine($"{globalKvp.Key}::({averageDamage:f2}/{averageHealth:f2}/{averageArmor:f2})");
+
+                foreach (var kvp in globalKvp.Value)
+                {
+                    Console.WriteLine($"-{kvp.Key} -> damage: {kvp.Value[0]}, health: {kvp.Value[1]}, armor: {kvp.Value[2]}");
+                }
+            }
+        }
+    }
+}
