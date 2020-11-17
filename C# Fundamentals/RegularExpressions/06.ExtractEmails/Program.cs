95af5333860166da21fcd1f7d43d0b0a7f428e03
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-11-17 14:44:26 +0200

    Regular Expressions - More Exercises, Extract Emails

---
+++ C# Fundamentals/RegularExpressions/06.ExtractEmails/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<=\s)[A-Za-z0-9]+([\.\-_]?[A-Za-z0-9]+)*@(?<host>[A-Za-z]+(\-?[A-Za-z]+)*\.[A-Za-z]+(\-?[A-Za-z]+)*(\.?[A-Za-z]+(\-?[A-Za-z])*)*)";
+
+            var matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
