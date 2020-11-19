680827e7b93cdb7143e8f9003e1d175c9707e800
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-11-19 18:24:42 +0200

    Text Processing - More Exercise, last 2 problems

---
+++ C# Fundamentals/TextProcessingAndRegexMoreExercises/04.MorseCodeTranslator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+
+namespace _04.MorseCodeTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder decryptedText = new StringBuilder();
+
+
+            foreach (string word in words)
+            {
+                string[] letters = word.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    char letter = FindTheLetter(letters[i]);
+                    decryptedText.Append(letter.ToString().ToUpper());
+                }
+
+                decryptedText.Append(" ");
+            }
+
+            Console.WriteLine(decryptedText.ToString().Trim());
+        }
+
+        static char FindTheLetter(string letterCode)
+        {
+            switch (letterCode)
+            {
+                case ".-": return 'a';
+                case "-...": return 'b';
+                case "-.-.": return 'c';
+                case "-..": return 'd';
+                case ".": return 'e';
+                case "..-.": return 'f';
+                case "--.": return 'g';
+                case "....": return 'h';
+                case "..": return 'i';
+                case ".---": return 'j';
+                case "-.-": return 'k';
+                case ".-..": return 'l';
+                case "--": return 'm';
+                case "-.": return 'n';
+                case "---": return 'o';
+                case ".--.": return 'p';
+                case "--.-": return 'q';
+                case ".-.": return 'r';
+                case "...": return 's';
+                case "-": return 't';
+                case "..-": return 'u';
+                case "...-": return 'v';
+                case ".--": return 'w';
+                case "-..-": return 'x';
+                case "-.--": return 'y';
+                case "--..": return 'z';
+                default: return ' ';
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/TextProcessingAndRegexMoreExercises/05.HTML/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace _05.HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string content = Console.ReadLine();
+
+            StringBuilder resultHtml = new StringBuilder();
+
+            resultHtml.AppendLine("<h1>");
+            resultHtml.AppendLine($"    {title}");
+            resultHtml.AppendLine("</h1>");
+
+            resultHtml.AppendLine("<article>");
+            resultHtml.AppendLine($"    {content}");
+            resultHtml.AppendLine("</article>");
+
+            string comment = string.Empty;
+
+            while ((comment = Console.ReadLine()) != "end of comments")
+            {
+                resultHtml.AppendLine("<div>");
+                resultHtml.AppendLine($"    {comment}");
+                resultHtml.AppendLine("</div>");
+            }
+
+            Console.WriteLine(resultHtml);
+        }
+    }
+}
