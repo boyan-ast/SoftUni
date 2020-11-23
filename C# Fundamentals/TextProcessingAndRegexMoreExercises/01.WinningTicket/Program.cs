16b36902fc45019b455826a1a5c7cd95a31dd571
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-11-23 19:33:15 +0200

    Regular Expressions - More Exercises, WinningTicket problem

---
+++ C# Fundamentals/TextProcessingAndRegexMoreExercises/01.WinningTicket/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.WinningTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allTickets = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string ticket in allTickets)
+            {
+                if (ticket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                }
+                else
+                {
+                    string jackpotPattern = @"(?<symbol>[@#$^])\1{9}";
+                    string firstHalf = ticket.Substring(0, 10);
+                    string secondHalf = ticket.Substring(10);
+
+                    if (Regex.IsMatch(firstHalf, jackpotPattern) && Regex.IsMatch(secondHalf, jackpotPattern))
+                    {
+                        Match match = Regex.Match(ticket, jackpotPattern);
+
+                        char winningSymbol = char.Parse(match.Groups["symbol"].Value);
+                        Console.WriteLine($"ticket \"{ticket}\" - 10{winningSymbol} Jackpot!");
+                    }
+                    else
+                    {
+                        char symbol = ' ';
+                        string allSymbols = string.Empty;
+                        bool isWinning = false;
+
+                        if (Regex.IsMatch(firstHalf, @"(?<allSymbols>(?<symbol>[@#$^])\k<symbol>{5,8})"))
+                        {
+                            Match match = Regex.Match(firstHalf, @"(?<allSymbols>(?<symbol>[@#$^])\k<symbol>{5,8})");
+
+                            symbol = char.Parse(match.Groups["symbol"].Value);
+                            allSymbols = match.Groups["allSymbols"].Value;
+
+                            if (secondHalf.Contains(allSymbols))
+                            {
+                                isWinning = true;
+                            }
+                        }
+
+                        if (isWinning)
+                        {
+                            Console.WriteLine($"ticket \"{ticket}\" - {allSymbols.Length}{symbol}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ticket \"{ticket}\" - no match");
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/TextProcessingAndRegexMoreExercises/01.WinningTicket_2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _01.WinningTicket_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder result = new StringBuilder();
+
+            string[] tickets = Console.ReadLine()
+                .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string pattern = @"(\@{6,}|\${6,}|\^{6,}|\#{6,})";
+            Regex rgx = new Regex(pattern);
+
+            foreach (string ticket in tickets)
+            {
+                if (ticket.Length != 20)
+                {
+                    result.Append($"invalid ticket{Environment.NewLine}");
+                    continue;
+                }
+
+                Match leftMatch = rgx.Match(ticket.Substring(0, 10));
+                Match rightMatch = rgx.Match(ticket.Substring(10));
+
+                if (!leftMatch.Success || !rightMatch.Success)
+                {
+                    result.Append($"ticket \"{ticket}\" - no match{Environment.NewLine}");
+                    continue;
+                }
+
+                int minLen = Math.Min(leftMatch.Value.Length, rightMatch.Value.Length);
+
+                string leftPart = leftMatch.Value.Substring(0, minLen);
+                string rightPart = rightMatch.Value.Substring(0, minLen);
+
+                if (leftPart.Equals(rightPart))
+                {
+                    if (leftPart.Length == 10)
+                    {
+                        result.Append($"ticket \"{ticket}\" - {minLen}{leftPart[0]} Jackpot!{Environment.NewLine}");
+                    }
+                    else
+                    {
+                        result.Append($"ticket \"{ticket}\" - {minLen}{leftPart[0]}{Environment.NewLine}");
+                    }
+                }
+                else
+                {
+                    result.Append($"ticket \"{ticket}\" - no match{Environment.NewLine}");
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
