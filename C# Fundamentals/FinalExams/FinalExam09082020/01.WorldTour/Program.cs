c00425f5fb94f1a562d72117060cfd3a51778d1f
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-12-01 14:53:31 +0200

    Final Exam 09082020, first two problems added

---
+++ C# Fundamentals/FinalExams/FinalExam09082020/01.WorldTour/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace _01.WorldTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stops = Console.ReadLine();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Travel")
+            {
+                string[] commandArgs = command
+                    .Split(":", StringSplitOptions.RemoveEmptyEntries);
+
+                string action = commandArgs[0];
+
+                if (action == "Add Stop")
+                {
+                    int index = int.Parse(commandArgs[1]);
+
+                    if (index >= 0 && index < stops.Length)
+                    {
+                        string newStop = commandArgs[2];
+                        stops = stops.Insert(index, newStop);
+                    }
+                }
+                else if (action == "Remove Stop")
+                {
+                    int startIndex = int.Parse(commandArgs[1]);
+                    int endIndex = int.Parse(commandArgs[2]);
+
+                    if (startIndex >= 0 && startIndex < stops.Length &&
+                        endIndex >= startIndex && endIndex < stops.Length)
+                    {
+                        stops = stops.Remove(startIndex, (endIndex - startIndex + 1));
+                    }
+                }
+                else if (action == "Switch")
+                {
+                    string oldStop = commandArgs[1];
+                    string newStop = commandArgs[2];
+
+                    if (stops.Contains(oldStop))
+                    {
+                        stops = stops.Replace(oldStop, newStop);
+                    }
+                }
+
+                Console.WriteLine(stops);
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+        }
+    }
+}
---
+++ C# Fundamentals/FinalExams/FinalExam09082020/02.DestinationMapper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.DestinationMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<=([=\/]))(?<place>[A-Z][A-Za-z]{2,})(?=\1)";
+
+            Regex rgx = new Regex(pattern);
+
+            var matches = rgx.Matches(input);
+
+            int points = matches.Select(x => x.Value).Sum(x => x.Length);
+
+            Console.WriteLine("Destinations: " + string.Join(", ", matches));
+            Console.WriteLine("Travel Points: {0}", points);
+        }
+    }
+}
