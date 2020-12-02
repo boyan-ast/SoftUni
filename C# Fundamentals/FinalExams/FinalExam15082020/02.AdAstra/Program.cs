492438f1b7aec247b7bf65b66d1acc3cbbb4b3cc
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-12-02 16:56:19 +0200

    Final Exam 15082020 - 2nd problem

--- C# Fundamentals/FinalExams/FinalExam15082020/02.AdAstra/Program.cs
+++ C# Fundamentals/FinalExams/FinalExam15082020/02.AdAstra/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace _02.AdAstra
 {
@@ -6,7 +8,27 @@ namespace _02.AdAstra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @"([|#])(?<item>[A-Za-z ]+)\1(?<date>\d{2}\/\d{2}\/\d{2})\1(?<cal>(\d{1,4}|10{4}))\1";
+
+            int totalCalories = 0;
+
+            var matches = Regex.Matches(Console.ReadLine(), pattern);
+
+            StringBuilder resultText = new StringBuilder();
+
+            foreach (Match match in matches)
+            {
+                resultText.AppendLine($"Item: {match.Groups["item"].Value}, Best before: {match.Groups["date"].Value}, " +
+                    $"Nutrition: {match.Groups["cal"].Value}");
+
+                totalCalories += int.Parse(match.Groups["cal"].Value);
+            }
+
+            int days = totalCalories / 2000;
+
+            resultText.Insert(0, $"You have food to last you for: {days} days!\n");
+
+            Console.WriteLine(resultText.ToString().Trim());
         }
     }
 }
