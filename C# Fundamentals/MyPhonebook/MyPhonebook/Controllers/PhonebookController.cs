8a898cb3ba9308d8090bcd60f143cc4f67af6714
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-12-10 18:02:53 +0200

    Basic Web Project - Phonebook from lab

---
+++ C# Fundamentals/MyPhonebook/MyPhonebook/Controllers/PhonebookController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyPhonebook.Models;
+
+namespace MyPhonebook.Controllers
+{
+    public class PhonebookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(DataContext.Contacts);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                DataContext.Contacts.Add(model);
+            }
+            else
+            {
+                TempData["Error"] = "Невалидни данни!";
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
