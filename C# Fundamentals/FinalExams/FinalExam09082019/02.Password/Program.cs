44cc8f9ac57c3595e6023dad252d4f33879633dc
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-12-12 11:13:05 +0200

    Final Exam 09082019 - 02.Password (Regex)

---
+++ C# Fundamentals/FinalExams/FinalExam09082019/02.Password/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^(.+)>(?<numbers>\d{3})\|(?<lowerLetters>[a-z]{3})\|(?<upperLetters>[A-Z]{3})\|(?<symbols>[^<>]{3})<\1$";
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match match = Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+                    string password = match.Groups["numbers"].Value +
+                        match.Groups["lowerLetters"] +
+                        match.Groups["upperLetters"] +
+                        match.Groups["symbols"];
+
+                    Console.WriteLine($"Password: {password}");
+                }
+                else
+                {
+                    Console.WriteLine("Try another password!");
+                }
+            }
+        }
+    }
+}
