298ee21a74375e07d1134b452b868131de0acba5
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-12-17 15:14:44 +0200

    Stacks and Queues - Lab problems

---
+++ C# Advanced/StacksAndQueues/04.MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> brackets = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    brackets.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int startIndex = brackets.Pop();
+
+                    string subexpression = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(subexpression);
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/StacksAndQueues/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+                        
+            Queue<int> queue = new Queue<int>();
+            Queue<int> evenNums = new Queue<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            while (queue.Count > 0)
+            {
+                int num = queue.Dequeue();
+
+                if (num % 2 == 0)
+                {
+                    evenNums.Enqueue(num);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", evenNums));
+        }
+    }
+}
