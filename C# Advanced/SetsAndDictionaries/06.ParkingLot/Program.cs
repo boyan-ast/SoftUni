bc6d85d558b826d90fb2be91ede489de9b19ed3f
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-12-22 17:52:37 +0200

    Sets and Dictionaries - 4 lab problems solved

---
+++ C# Advanced/SetsAndDictionaries/06.ParkingLot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carNumbers = new HashSet<string>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] carInfo = command.Split(", ");
+
+                if (carInfo[0] == "IN")
+                {
+                    carNumbers.Add(carInfo[1]);
+                }
+                else if (carInfo[0] == "OUT")
+                {
+                    carNumbers.Remove(carInfo[1]);
+                }
+            }
+
+            if (carNumbers.Count > 0)
+            {
+                foreach (string car in carNumbers)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ C# Advanced/SetsAndDictionaries/07.SoftUniParty/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> regularGuests = new HashSet<string>();
+            HashSet<string> vipGuests = new HashSet<string>();
+
+            string guest = string.Empty;
+
+            while ((guest = Console.ReadLine()) != "PARTY")
+            {
+                if (char.IsDigit(guest[0]))
+                {
+                    vipGuests.Add(guest);
+                }
+                else
+                {
+                    regularGuests.Add(guest);
+                }
+            }
+
+            while ((guest = Console.ReadLine()) != "END")
+            {
+                if (char.IsDigit(guest[0]))
+                {
+                    vipGuests.Remove(guest);
+                }
+                else
+                {
+                    regularGuests.Remove(guest);
+                }
+            }
+
+            vipGuests.UnionWith(regularGuests);
+
+            Console.WriteLine(vipGuests.Count);
+
+            foreach (string person in vipGuests)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
