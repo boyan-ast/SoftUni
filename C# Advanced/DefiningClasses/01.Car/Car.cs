0a228361ac6f802ef18861c881dc6f31e8fcc362
Boyan Astardzhiev <boyan_gb@hotmail.com>
2020-12-30 12:30:40 +0200

    Defining Classes - Lab, the first 4 problems solved

---
+++ C# Advanced/DefiningClasses/01.Car/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, 
+            string model, 
+            int year, 
+            double fuelQuantity, 
+            double fuelConsumption) : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make,
+            string model,
+            int year,
+            double fuelQuantity,
+            double fuelConsumption,
+            Engine engine,
+            Tire[] tires) : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            Engine = engine;
+            Tires = tires;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - distance * FuelConsumption >= 0)
+            {
+                FuelQuantity -= distance * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Make: {Make}");
+            result.AppendLine($"Model: {Model}");
+            result.AppendLine($"Year: {Year}");
+            result.Append($"Fuel: {FuelQuantity:f2}L");
+
+            return result.ToString();
+        }
+    }
+}
---
+++ C# Advanced/DefiningClasses/01.Car/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var tires = new Tire[4]
+            {
+                new Tire(2018, 1.4),
+                new Tire(2018, 1.4),
+                new Tire(2018, 1.4),
+                new Tire(2018, 1.4)
+            };
+
+            Engine engine = new Engine(90, 1.6);
+
+            Car car = new Car("Ford", "Focus", 2007, 60, 4.2, engine, tires);
+         }
+    }
+}
