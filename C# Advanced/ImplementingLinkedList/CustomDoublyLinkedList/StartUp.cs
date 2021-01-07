da1d427cebe13615444d6737ba4b73f21b7b3a63
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-01-07 16:39:02 +0200

    Implementing custom doubly linked list

---
+++ C# Advanced/ImplementingLinkedList/CustomDoublyLinkedList/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.AddFirst(i);
+            }
+
+            int[] array = list.ToArray();
+        }
+    }
+}
