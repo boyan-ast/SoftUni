1397fb6f690bb57302a7d889ed31d93eb632a01d
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-01-08 11:37:52 +0200

    Implementing custom list

---
+++ C# Advanced/ImplementingCustomDataStructures/ImplementingCustomList/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ImplementingCustomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomList list = new CustomList();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i * 10);
+            }
+
+            list.RemoveAt(1);
+            list.Insert(4, 123);
+            list.Insert(3, 345);
+
+            list.Swap(1, 3);
+
+            list.Swap(1, 1);
+
+            Console.WriteLine(list);
+        }
+    }
+}
