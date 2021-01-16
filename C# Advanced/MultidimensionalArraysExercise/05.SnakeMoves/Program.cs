34d7173cd1af2539eb3986791d28b48197407f01
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-01-16 15:32:53 +0200

    Multidimensional Arrays - Exercise, 2 problems solved

---
+++ C# Advanced/MultidimensionalArraysExercise/05.SnakeMoves/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndColsData = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            char[,] matrix = new char[rowsAndColsData[0], rowsAndColsData[1]];
+
+            Queue<char> snake = new Queue<char>(Console.ReadLine());
+
+            int row = 0;
+            int col = 0;
+
+            string direction = "right";
+
+            while (true)
+            {
+                char symbol = snake.Dequeue();
+                matrix[row, col] = symbol;
+                snake.Enqueue(symbol);                
+
+                if (direction == "right")
+                {
+                    col++;
+
+                    if (col == matrix.GetLength(1))
+                    {
+                        col--;
+                        row++;
+
+                        direction = "left";
+                    }
+                }
+                else if (direction == "left")
+                {
+                    col--;
+
+                    if (col < 0)
+                    {
+                        col++;
+                        row++;
+
+                        direction = "right";
+                    }
+                }
+
+                if (row == matrix.GetLength(0))
+                {
+                    break;
+                }
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/MultidimensionalArraysExercise/06.JaggedArrayManipulation/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.JaggedArrayManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[][] matrix = new double[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                matrix[row] = new double[rowData.Length];
+
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    matrix[row][col] = rowData[col];
+                }
+            }
+
+            for (int row = 0; row < n - 1; row++)
+            {
+                if (matrix[row].Length == matrix[row+1].Length)
+                {
+                    matrix[row] = matrix[row].Select(x => { x = x * 2; return x; }).ToArray();
+                    matrix[row + 1] = matrix[row + 1].Select(x => { x = x * 2; return x; }).ToArray();
+                }
+                else
+                {
+                    matrix[row] = matrix[row].Select(x => { x = x / 2; return x; }).ToArray();
+                    matrix[row + 1] = matrix[row + 1].Select(x => { x = x / 2; return x; }).ToArray();
+                }
+            }
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] commandArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string action = commandArgs[0];
+                int row = int.Parse(commandArgs[1]);
+                int col = int.Parse(commandArgs[2]);
+                int value = int.Parse(commandArgs[3]);
+
+                if (row < 0 || row >= n || col >= matrix[row].Length || col < 0)
+                {
+                    continue;
+                }
+
+                if (action == "Add")
+                {
+                    matrix[row][col] += value;
+                }
+                else if (action == "Subtract")
+                {
+                    matrix[row][col] -= value;
+                }
+
+            }
+
+            // Printing the matrix
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write(matrix[row][col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
