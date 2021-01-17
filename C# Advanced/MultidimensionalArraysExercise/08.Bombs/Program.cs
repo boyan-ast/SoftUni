976866aad4a21d7cfed4ff4832e73034e9882606
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-01-17 17:24:06 +0200

    Multidimensional Arrays - Exercise, two exam problems solved

---
+++ C# Advanced/MultidimensionalArraysExercise/08.Bombs/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            string[] bombsDataAsString = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int[,] bombsData = new int[bombsDataAsString.Length, 2];
+            int count = 0;
+
+            foreach (string bomb in bombsDataAsString)
+            {
+                int row = int.Parse(bomb.Split(",", StringSplitOptions.RemoveEmptyEntries)[0]);
+                int col = int.Parse(bomb.Split(",", StringSplitOptions.RemoveEmptyEntries)[1]);
+                bombsData[count, 0] = row;
+                bombsData[count, 1] = col;
+                count++;
+            }
+
+            for (int i = 0; i < bombsData.GetLength(0); i++)
+            {
+                int row = bombsData[i, 0];
+                int col = bombsData[i, 1];
+
+                int power = matrix[row, col];
+
+                if (power <= 0)
+                {
+                    continue;
+                }
+
+                matrix[row, col] = 0;
+
+                if (IsValidCell(row - 1, col - 1, n) && matrix[row - 1, col - 1] > 0)
+                {
+                    matrix[row - 1, col - 1] -= power;
+                }
+                if (IsValidCell(row - 1, col, n) && matrix[row - 1, col] > 0)
+                {
+                    matrix[row - 1, col] -= power;
+                }
+                if (IsValidCell(row - 1, col + 1, n) && matrix[row - 1, col + 1] > 0)
+                {
+                    matrix[row - 1, col + 1] -= power;
+                }
+                if (IsValidCell(row, col - 1, n) && matrix[row, col - 1] > 0)
+                {
+                    matrix[row, col - 1] -= power;
+                }
+                if (IsValidCell(row, col + 1, n) && matrix[row, col + 1] > 0)
+                {
+                    matrix[row, col + 1] -= power;
+                }
+                if (IsValidCell(row + 1, col - 1, n) && matrix[row + 1, col - 1] > 0)
+                {
+                    matrix[row + 1, col - 1] -= power;
+                }
+                if (IsValidCell(row + 1, col, n) && matrix[row + 1, col] > 0)
+                {
+                    matrix[row + 1, col] -= power;
+                }
+                if (IsValidCell(row + 1, col + 1, n) && matrix[row + 1, col + 1] > 0)
+                {
+                    matrix[row + 1, col + 1] -= power;
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {CalculateCountAndSum(matrix).Item1}");
+            Console.WriteLine($"Sum: {CalculateCountAndSum(matrix).Item2}");
+            PrintMatrix(matrix);
+        }
+
+        private static Tuple<int, int> CalculateCountAndSum(int[,] matrix)
+        {
+            int count = 0;
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        count++;
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+
+            return Tuple.Create(count, sum);
+        }
+
+        static bool IsValidCell(int row, int col, int n)
+        {
+            if (row < 0 || row >= n || col < 0 || col >= n)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/MultidimensionalArraysExercise/09.Miner/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            char[,] field = new char[n, n];
+            int coals = 0;
+            int row = -1;
+            int col = -1;
+
+            for (int i = 0; i < n; i++)
+            {
+                char[] rowData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < n; j++)
+                {
+                    field[i, j] = rowData[j];
+
+                    if (field[i, j] == 'c')
+                    {
+                        coals++;
+                    }
+                    else if (field[i, j] == 's')
+                    {
+                        row = i;
+                        col = j;
+                    }
+                }
+            }
+
+            int collectedCoals = 0;
+
+            for (int i = 0; i < commands.Length; i++)
+            {
+                string command = commands[i];
+
+                if (command == "right" && (col + 1 < n))
+                {
+                    field[row, col] = '*';
+
+                    col++;
+
+                    if (field[row, col] == 'c')
+                    {
+                        collectedCoals++;
+                        coals--;
+
+                        if (coals == 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({row}, {col})");
+                            return;
+                        }
+                    }
+                    else if (field[row, col] == 'e')
+                    {
+                        Console.WriteLine($"Game over! ({row}, {col})");
+                        return;
+                    }
+
+                    field[row, col] = 's';
+                }
+                else if (command == "left" && (col - 1 >= 0))
+                {
+                    field[row, col] = '*';
+                    col--;
+
+                    if (field[row, col] == 'c')
+                    {
+                        collectedCoals++;
+                        coals--;
+
+                        if (coals == 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({row}, {col})");
+                            return;
+                        }
+                    }
+                    else if (field[row, col] == 'e')
+                    {
+                        Console.WriteLine($"Game over! ({row}, {col})");
+                        return;
+                    }
+
+                    field[row, col] = 's';
+                }
+                else if (command == "down" && (row + 1 < n))
+                {
+                    field[row, col] = '*';
+                    row++;
+
+                    if (field[row, col] == 'c')
+                    {
+                        collectedCoals++;
+                        coals--;
+
+                        if (coals == 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({row}, {col})");
+                            return;
+                        }
+                    }
+                    else if (field[row, col] == 'e')
+                    {
+                        Console.WriteLine($"Game over! ({row}, {col})");
+                        return;
+                    }
+
+                    field[row, col] = 's';
+                }
+                else if (command == "up" && (row - 1 >= 0))
+                {
+                    field[row, col] = '*';
+                    row--;
+
+                    if (field[row, col] == 'c')
+                    {
+                        collectedCoals++;
+                        coals--;
+
+                        if (coals == 0)
+                        {
+                            Console.WriteLine($"You collected all coals! ({row}, {col})");
+                            return;
+                        }
+                    }
+                    else if (field[row, col] == 'e')
+                    {
+                        Console.WriteLine($"Game over! ({row}, {col})");
+                        return;
+                    }
+                    field[row, col] = 's';
+                }
+
+                //Console.WriteLine("------------------------------");
+                //PrintMatrix(field);
+            }
+
+            Console.WriteLine($"{coals} coals left. ({row}, {col})");
+
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
