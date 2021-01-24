daabd50f470d687e5cd6a98e3d1eab2ced18bd34
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-01-24 13:50:07 +0200

    Streams and Files - Recursive Directories problem

---
+++ C# Advanced/StreamsFilesDirectories/07.RecursiveDirectories_Bonus/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace _07.RecursiveDirectories_Bonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"D:\Games";
+            Console.WriteLine(ScanFolderRecursively(folderPath, 0));
+        }
+
+        static double ScanFolderRecursively(string folderPath, int identation)
+        {
+            string[] filePaths = Directory.GetFiles(folderPath);
+            double size = 0;
+
+            foreach (string file in filePaths)
+            {
+                FileInfo info = new FileInfo(file);
+                Console.WriteLine($"{new string(' ', identation)}{info.FullName}");
+
+                size += info.Length;
+            }
+
+            string[] directories = Directory.GetDirectories(folderPath);
+
+            foreach (string directory in directories)
+            {
+                 size += ScanFolderRecursively(directory, identation + 4);
+            }      
+            
+
+            return size / 1024.00 / 1024.00;
+        }
+    }
+}
