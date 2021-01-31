0648c72c93d2147053ebd6d5fea4afaba7109fe8
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-01-31 15:18:59 +0200

    Classes - Exercise, SoftUniParking problem solved

---
+++ C# Advanced/DefininigClassesExercise/10.SoftUniParking/Parking.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private int capacity;
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            Cars = new List<Car>(capacity);
+        }
+
+        public List<Car> Cars { get; set; }
+        public int Count
+        {
+            get
+            {
+                return Cars.Count;
+            }
+        }
+
+        public string AddCar(Car car)
+        {
+            if (CarExists(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            else if (Cars.Count == capacity)
+            {
+                return "Parking is full!";
+            }
+            else
+            {
+                Cars.Add(car);
+                return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+            }
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (!CarExists(registrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            else
+            {
+                Cars.RemoveAll(c => c.RegistrationNumber == registrationNumber);
+                return $"Successfully removed {registrationNumber}";
+            }
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            return Cars.FirstOrDefault(c => c.RegistrationNumber == registrationNumber);
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (string number in registrationNumbers)
+            {
+                RemoveCar(number);
+            }
+        }
+
+        private bool CarExists(string registrationNumber)
+        {
+            return Cars.Any(c => c.RegistrationNumber == registrationNumber);
+        }
+    }
+}
---
+++ C# Advanced/DefininigClassesExercise/10.SoftUniParking/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUniParking
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var car = new Car("Skoda", "Fabia", 65, "CC1856BG");
+            var car2 = new Car("Audi", "A3", 110, "EB8787MN");
+            var car3 = new Car("Ford", "Focus", 110, "EB7918BB");
+            var car4 = new Car("Volga", "100", 200, "EB7918AB"); 
+            var car5 = new Car("Daewoo", "Tiko", 200, "EB8018AB");
+            var car6 = new Car("Daihatsu", "147", 200, "EB9018AB");
+
+            Console.WriteLine(car.ToString());
+            //Make: Skoda
+            //Model: Fabia
+            //HorsePower: 65
+            //RegistrationNumber: CC1856BG
+
+            var parking = new Parking(5);
+            Console.WriteLine(parking.AddCar(car));
+            //Successfully added new car Skoda CC1856BG
+
+            Console.WriteLine(parking.AddCar(car));
+            //Car with that registration number, already exists!
+
+            Console.WriteLine(parking.AddCar(car2));
+            //Successfully added new car Audi EB8787MN
+
+            Console.WriteLine(parking.GetCar("EB8787MN").ToString());
+            //Make: Audi
+            //Model: A3
+            //HorsePower: 110
+            //RegistrationNumber: EB8787MN
+            Console.WriteLine(parking.AddCar(car3));
+            Console.WriteLine(parking.AddCar(car4));
+            Console.WriteLine(parking.AddCar(car5));
+            Console.WriteLine(parking.AddCar(car6));
+
+            List<string> numbersToRemove = new List<string> { "EB8018AB", "EB7918AB" };
+
+            parking.RemoveSetOfRegistrationNumber(numbersToRemove);
+
+            //Console.WriteLine(parking.RemoveCar("EB8787MN"));
+            //Successfullyremoved EB8787MN
+
+            Console.WriteLine(parking.Count); //1
+        }
+    }
+}
