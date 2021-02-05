f82705606cdeeab5ad6f5f20f403f809bfa17bb9
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-02-05 13:29:54 +0200

    Implementing Custom List

---
+++ C# Advanced/ImplementingCustomList/ImplementingCustomList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ImplementingCustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoftUniList myList = new SoftUniList();
+
+            myList.Add(10);
+            myList.Add(20);
+            myList.Add(30);
+            myList.Add(40);
+            myList.Add(50);
+            myList.Add(60);
+            myList.Add(70);
+            myList.Add(80);
+            myList.Add(90);
+
+            PrintList(myList);
+
+            Console.WriteLine(myList.RemoveAt(0)); 
+            PrintList(myList);
+            myList.RemoveAt(0);
+            PrintList(myList);
+            myList.RemoveAt(0);
+            PrintList(myList);
+            myList.RemoveAt(0);
+            PrintList(myList);
+
+            Console.WriteLine(myList.Contains(10)); 
+            Console.WriteLine(myList.Contains(90));
+
+            myList.InsertAt(2, 99);
+            PrintList(myList);
+
+            myList.InsertAt(6, 77);
+            PrintList(myList);
+            Console.WriteLine(myList.Count);
+
+            myList.Swap(0, myList.Count - 1);
+            PrintList(myList);
+        }
+
+        private static void PrintList(SoftUniList myList)
+        {
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.Write(myList[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C# Advanced/ImplementingCustomList/ImplementingCustomList/SoftUniList.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementingCustomList
+{
+    public class SoftUniList
+    {
+        private const int InitialCapacity = 2;
+
+        private int[] items;
+
+        public SoftUniList()
+        {
+            this.items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+                return this.items[index];
+            }
+            set
+            {
+                ValidateIndex(index);
+                this.items[index] = value;
+            }
+        }
+
+        public void Add(int element)
+        {
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+
+            this.items[this.Count] = element;
+            this.Count++;
+        }
+
+        public int RemoveAt(int index)
+        {
+            ValidateIndex(index);
+
+            int removedElement = this.items[index];
+
+            this.items[index] = default(int);
+            this.ShiftLeft(index);
+
+            if (this.Count <= this.items.Length / 4)
+            {
+                this.Shrink();
+            }
+            
+            return removedElement;
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.items[i] == element)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void InsertAt(int index, int element)
+        {
+            if (index > this.Count)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index");
+            }
+            else if (index == this.Count)
+            {
+                this.Add(element);
+            }
+            else
+            {
+                if (this.Count == this.items.Length)
+                {
+                    this.Resize();
+                }
+
+                this.ShiftRight(index);
+
+                this.items[index] = element;
+            }            
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            ValidateIndex(firstIndex);
+            ValidateIndex(secondIndex);
+
+            int temp = this.items[firstIndex];
+            this.items[firstIndex] = this.items[secondIndex];
+            this.items[secondIndex] = temp;
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int i = this.Count; i > index; i--)
+            {
+                this.items[i] = this.items[i - 1];
+            }
+
+            this.Count++;
+        }
+
+        private void Shrink()
+        {
+            int[] copy = new int[this.items.Length / 2];
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                copy[i] = this.items[i];
+            }
+
+            this.items = copy;
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+
+            this.items[this.Count - 1] = default(int);
+
+            this.Count--;
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[items.Length * 2];
+
+            Array.Copy(this.items, copy, Count);
+
+            this.items = copy;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index");
+            }
+        }
+    }
+}
