d262569c157346532c34666bcf655c215943d52d
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-02-08 14:05:57 +0200

    Implementing custom generic stack

---
+++ C# Advanced/ImplementingCustomStack/CustomGenericStack/SoftUniStack.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomGenericStack
+{
+    public class SoftUniStack<T>
+    {
+        private const int InitialCapacity = 4;
+        private T[] items;
+
+        public SoftUniStack()
+        {
+            this.Count = 0;
+            this.items = new T[InitialCapacity];
+        }
+
+        public int Count
+        {
+            get;
+            private set;
+        }
+
+        public void Push(T element)
+        {
+            if (this.Count == this.items.Length)
+            {
+                this.Resize();
+            }
+
+            this.items[this.Count] = element;
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            T element = this.items[this.Count - 1];
+            this.Count--;
+            this.items[this.Count] = default(T);
+
+            if (this.items.Length >= 4 && this.Count <= this.items.Length / 4)
+            {
+                this.Shrink();
+            }
+
+            return element;
+        }
+
+        public T Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            return this.items[this.Count - 1];
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                action(this.items[i]);
+            }
+        }
+
+        private void Shrink()
+        {
+            T[] copy = new T[this.items.Length / 2];
+            Array.Copy(this.items, copy, Count);
+            this.items = copy;
+        }
+
+        private void Resize()
+        {
+            T[] copy = new T[this.items.Length * 2];
+
+            Array.Copy(this.items, copy, Count);
+
+            this.items = copy;
+        }
+    }
+}
---
+++ C# Advanced/ImplementingCustomStack/CustomGenericStack/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CustomGenericStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniStack<string> myStack = new SoftUniStack<string>();
+            myStack.Push("Ivan Vachev");
+            myStack.Push("Vanio Ivanov");
+            myStack.Push("Iliya Munin");
+            myStack.Push("Tsar Ivan");
+            myStack.Push("Misyak");
+            myStack.Push("Patrick-Gabriel Galchev");
+
+            //while (myStack.Count >= 2)
+            //{
+            //    Console.WriteLine(myStack.Pop());
+            //}
+
+            //Console.WriteLine(myStack.Peek());
+
+            myStack.ForEach(e => Console.WriteLine("The name is " + e));
+        }
+    }
+}
