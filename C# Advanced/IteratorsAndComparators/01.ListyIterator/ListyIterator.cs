cf8554899a7da85cee1bda29cb0fdb2e51f0c0b1
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-02-11 12:58:06 +0200

    Iterators and Comparators, ListyIterator problem

---
+++ C# Advanced/IteratorsAndComparators/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> elements;
+        private int index = 0;
+
+        public ListyIterator(params T[] elements)
+        {
+            this.elements = new List<T>(elements);
+        }
+
+        public bool Move()
+        {
+            return ++index < elements.Count;
+        }
+
+        public bool HasNext()
+        {
+            return index < elements.Count;
+        }
+
+        public void Print()
+        {
+            if (elements.Count == 0 || !HasNext())
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(elements[index]);
+        }
+    }
+}
---
+++ C# Advanced/IteratorsAndComparators/01.ListyIterator/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            string[] commandArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] items = commandArgs.Skip(1).ToArray();
+            ListyIterator<string> list = new ListyIterator<string>(items);
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "Move")
+                {
+                    Console.WriteLine(list.Move());
+                }
+                else if (command == "Print")
+                {
+                    try
+                    {
+                        list.Print();
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        Console.WriteLine("Invalid Operation!");                        
+                    }                    
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(list.HasNext());
+                }
+            }
+        }
+    }
+}
