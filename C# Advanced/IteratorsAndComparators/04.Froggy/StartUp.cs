b63732fc3474a81f80eb6ae85af5c78fbb39b65c
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-02-11 14:51:29 +0200

    Iterators and Comparators - Exercise, two more problems solved

---
+++ C# Advanced/IteratorsAndComparators/04.Froggy/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Lake stones = new Lake(numbers);
+            int index = -1;
+
+            foreach (int stone in stones)
+            {
+                index++;
+                if (index < numbers.Length - 1)
+                {
+                    Console.Write(stone + ", ");
+                }
+                else
+                {
+                    Console.WriteLine(stone);
+                }                
+            }
+        }
+    }
+}
---
+++ C# Advanced/IteratorsAndComparators/05.ComparingObjects/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] commandArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(commandArgs[0], int.Parse(commandArgs[1]), commandArgs[2]);
+                people.Add(person);
+            }
+
+            int n = int.Parse(Console.ReadLine());
+
+            Person theMan = people[n - 1];
+
+            int equal = 0;
+            int notEqual = 0;
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                Person currentPerson = people[i];
+
+                if (currentPerson.CompareTo(theMan) == 0)
+                {
+                    equal++;
+                }
+                else
+                {
+                    notEqual++;
+                }
+            }
+
+            if (equal != 1)
+            {
+                Console.WriteLine($"{equal} {notEqual} {people.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
