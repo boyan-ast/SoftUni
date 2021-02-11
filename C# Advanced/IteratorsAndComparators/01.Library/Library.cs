14ca86c503dcf9aa69aa238de969954ae55340c1
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-02-11 12:19:46 +0200

    Iterators and Comparators, lab problems solved

---
+++ C# Advanced/IteratorsAndComparators/01.Library/Library.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books, new BookComparator());
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book> 
+        {
+            private int index = -1;
+            private readonly List<Book> items;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                Reset();
+                items = new List<Book>(books);
+            }
+
+            public Book Current => items[index];
+
+            object IEnumerator.Current => items[index];
+
+            public void Dispose()
+            {
+                
+            }
+
+            public bool MoveNext()
+            {
+                return ++index < items.Count;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+        }
+
+    }
+}
+
+
---
+++ C# Advanced/IteratorsAndComparators/01.Library/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 1930, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+            List<Book> orderedBooks = new List<Book>
+            {
+                bookOne,
+                bookTwo,
+                bookThree
+            };
+
+            orderedBooks.Sort();
+
+            foreach (Book book in libraryTwo)
+            {
+                Console.WriteLine($"{book.Title} {book.Year}");
+            }
+        }
+    }
+}
