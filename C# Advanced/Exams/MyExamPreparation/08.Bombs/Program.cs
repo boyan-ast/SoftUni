ebe47a7b14a57f0f36d122c00e55b1eacda8c06c
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-02-18 17:28:50 +0200

    My Exam Preparation - two problems solved

---
+++ C# Advanced/Exams/MyExamPreparation/08.Bombs/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = ReadMatrix(n);
+
+            string[] bombsData = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int[][] bombs = new int[bombsData.Length][];
+
+            for (int i = 0; i < bombsData.Length; i++)
+            {
+                int[] bombInfo = bombsData[i]
+                    .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int[] bomb = new int[] { bombInfo[0], bombInfo[1] };
+
+                bombs[i] = bomb;
+            }
+
+            foreach (int[] bomb in bombs)
+            {
+                int row = bomb[0];
+                int col = bomb[1];
+
+                if (matrix[row, col] > 0)
+                {
+                    Explode(matrix, row, col);
+                }
+            }
+
+            int count = 0;
+            int sum = 0;
+
+            foreach (int number in matrix)
+            {
+                if (number > 0)
+                {
+                    count++;
+                    sum += number;
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {count}");
+            Console.WriteLine($"Sum: {sum}");
+
+            PrintMatrix(matrix);
+        }
+
+        private static void Explode(int[,] matrix, int row, int col)
+        {
+            int value = matrix[row, col];
+            matrix[row, col] = 0;
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i != 0 || j != 0)
+                    {
+                        if (IsValidPosition(matrix.GetLength(0), row + i, col + j)
+                            && matrix[row + i, col + j] > 0)
+                        {
+                            matrix[row + i, col + j] -= value;
+                        }
+                    }
+                }
+            }
+        }
+
+        static bool IsValidPosition(int n, int row, int col)
+        {
+            if (row < 0 || col < 0 || row >= n || col >= n)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static int[,] ReadMatrix(int n)
+        {
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            return matrix;
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exams/MyExamPreparation/10.RadioactiveMutantVampireBunnies/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.RadioactiveMutantVampireBunnies
+{
+    class Program
+    {
+        static bool isWinner = false;
+        static bool isDead = false;
+
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = sizes[0];
+            int m = sizes[1];
+
+            char[,] matrix = ReadMatrix(n, m);
+
+            int row = -1;
+            int col = -1;
+
+            // Find the player
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i, j] == 'P')
+                    {
+                        row = i;
+                        col = j;
+                    }
+                }
+            }
+
+            string moves = Console.ReadLine();
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+                char command = moves[i];
+
+                // Move Player
+
+                int nextRow = row;
+                int nextCol = col;
+
+                matrix[row, col] = '.';
+
+                if (command == 'U')
+                {
+                    nextRow--;
+                }
+                else if (command == 'D')
+                {
+                    nextRow++;
+                }
+                else if (command == 'L')
+                {
+                    nextCol--;
+                }
+                else if (command == 'R')
+                {
+                    nextCol++;
+                }
+
+                if (!IsValidPosition(n, m, nextRow, nextCol))
+                {
+                    isWinner = true;
+                }
+                else if (matrix[nextRow, nextCol] == 'B')
+                {
+                    isDead = true;
+                }
+
+                if (isWinner)
+                {
+                    SpreadBunnies(matrix);
+                    break;
+                }
+
+                row = nextRow;
+                col = nextCol;
+
+                if (!isDead)
+                {
+                    matrix[row, col] = 'P';
+                }
+
+                SpreadBunnies(matrix);
+
+                if (isDead)
+                {
+                    break;
+                }
+            }
+
+            PrintMatrix(matrix);
+
+            Console.Write(isWinner ? "won: " : "dead: ");
+            Console.WriteLine($"{row} {col}");
+
+        }
+
+        private static void SpreadBunnies(char[,] matrix)
+        {
+            int n = matrix.GetLength(0);
+            int m = matrix.GetLength(1);
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < m; col++)
+                {
+                    if (matrix[row, col] == 'B')
+                    {
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                if (Math.Abs(i) != Math.Abs(j)
+                                    && IsValidPosition(n, m, row + i, col + j)
+                                    && matrix[row + i, col + j] != 'B')
+                                {
+                                    if (matrix[row + i, col + j] == 'P')
+                                    {
+                                        isDead = true;
+                                    }
+
+                                    matrix[row + i, col + j] = 'b';
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            TheNewBunniesGrow(matrix);
+        }
+
+        private static void TheNewBunniesGrow(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 'b')
+                    {
+                        matrix[row, col] = 'B';
+                    }
+                }
+            }
+        }
+
+        static bool IsValidPosition(int n, int m, int row, int col)
+        {
+            if (row < 0 || col < 0 || row >= n || col >= m)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static char[,] ReadMatrix(int n, int m)
+        {
+            char[,] matrix = new char[n, m];
+
+            for (int row = 0; row < n; row++)
+            {
+                string rowData = Console.ReadLine();
+
+                for (int col = 0; col < m; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
