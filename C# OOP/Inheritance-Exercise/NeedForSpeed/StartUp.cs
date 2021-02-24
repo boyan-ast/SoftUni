e0b06317f804e67a75a0111638c9aaf7e0c2f3ef
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-02-24 18:43:47 +0200

    Inheritance - Exercise, Need For Speed problem solved

--- C# OOP/Inheritance-Exercise/NeedForSpeed/StartUp.cs
+++ C# OOP/Inheritance-Exercise/NeedForSpeed/StartUp.cs
@@ -1,10 +1,23 @@
-﻿namespace NeedForSpeed
+﻿using System;
+
+namespace NeedForSpeed
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            Car car = new Car(101, 55);
+
+            car.Drive(100);
+
+            Console.WriteLine(car.Fuel);
+
+            RaceMotorcycle mator = new RaceMotorcycle(200, 50);
+
+            mator.Drive(100);
+
+            Console.WriteLine(mator.Fuel);
+
         }
     }
 }
