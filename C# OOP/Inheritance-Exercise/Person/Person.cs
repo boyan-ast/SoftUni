dec28494d08d7ca5cbd02a84d57291b4eea44ba5
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-02-24 15:20:04 +0200

    Inheritance - Exercise, 3 problems solved

---
+++ C# OOP/Inheritance-Exercise/Person/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
---
+++ C# OOP/Inheritance-Exercise/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(new Hero("basicHero", 1));
+            Console.WriteLine(new Elf("elfka", 12));
+            Console.WriteLine(new BladeKnight("hurricane", 123));
+        }
+    }
+}
