a31f3a17a4e150b7c1f54c7c71e3a2ac0da3c726
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-02-25 13:52:54 +0200

    Inheritance - Exercise, two problems solved

---
+++ C# OOP/Inheritance-Exercise/Animals/Animal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, int age, string gender)
+        {
+            if (age <= 0)
+            {
+                throw new Exception("Invalid input!");
+            }
+            if (gender != "Male" && gender != "Female")
+            {
+                throw new Exception("Invalid input!");
+            }
+
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public virtual string ProduceSound()
+        {
+            return default;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"{GetType().Name}");
+            result.AppendLine($"{Name} {Age} {Gender}");
+            result.Append($"{ProduceSound()}");
+
+            return result.ToString();
+        }
+    }
+}
--- C# OOP/Inheritance-Exercise/Animals/StartUp.cs
+++ C# OOP/Inheritance-Exercise/Animals/StartUp.cs
@@ -1,10 +1,109 @@
-﻿namespace Animals
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+
+            string animalType = string.Empty;
+            List<Animal> animals = new List<Animal>();
+
+            while ((animalType = Console.ReadLine()) != "Beast!")
+            {
+                string[] animalInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = string.Empty;
+                int age = 0;
+                string gender = string.Empty;
+
+                try
+                {
+                    name = animalInfo[0];
+                    age = int.Parse(animalInfo[1]);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                if (animalInfo.Length > 2)
+                {
+                    gender = animalInfo[2];
+                }
+
+                switch (animalType)
+                {
+                    case "Cat":
+                        try
+                        {
+                            Cat cat = new Cat(name, age, gender);
+                            animals.Add(cat);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "Dog":
+                        try
+                        {
+                            Dog dog = new Dog(name, age, gender);
+                            animals.Add(dog);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "Frog":
+                        try
+                        {
+                            Frog frog = new Frog(name, age, gender);
+                            animals.Add(frog);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "Kitten":
+                        try
+                        {
+                            Kitten kitten = new Kitten(name, age);
+                            animals.Add(kitten);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "Tomcat":
+                        try
+                        {
+                            Tomcat tomcat = new Tomcat(name, age);
+                            animals.Add(tomcat);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input!");
+                        break;
+                }
+
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
         }
     }
 }
