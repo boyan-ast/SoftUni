09a4b754f81ca2cf2c0f6d82bff1e957b68f9c46
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-03-03 12:24:38 +0200

    Interfaces and Abstraction, Lab problems solved

---
+++ C# OOP/InterfacesAndAbstraction-Lab/Cars/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cars
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ICar seat = new Seat("Ibiza", "Red");
+            ICar tesla = new Tesla("Model 7", "White", 2);
+
+            List<ICar> cars = new List<ICar>() { seat, tesla };
+
+            foreach (ICar car in cars)
+            {
+                Console.WriteLine(car.ToString());
+                Console.WriteLine(car.Start());
+                Console.WriteLine(car.Stop()); 
+            }
+        }
+    }
+}
---
+++ C# OOP/InterfacesAndAbstraction-Lab/Shapes/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int radius = int.Parse(Console.ReadLine());
+
+            IDrawable circle = new Circle(radius);
+
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            IDrawable rectangle = new Rectangle(width, height);
+
+            List<IDrawable> drawableObjects = new List<IDrawable>()
+            {
+                circle,
+                rectangle
+            };
+
+            foreach (IDrawable drawable in drawableObjects)
+            {
+                drawable.Draw();
+            }
+        }
+    }
+}
