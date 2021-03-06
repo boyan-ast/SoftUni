3b441e8d05a6847e80597dfa7216f017b175c613
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-03-06 14:21:39 +0200

    Polymorphism - Lab problems solved

---
+++ C# OOP/Polymorphism-Lab/Operations/MathOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int firstNum, int secondNum) => firstNum + secondNum;
+
+        public double Add(double firstNum, double secondNum, double thirdNum) 
+            => firstNum + secondNum + thirdNum;
+
+        public decimal Add(decimal firstNum, decimal secondNum, decimal thirdNum)
+            => firstNum + secondNum + thirdNum;
+    }
+}
---
+++ C# OOP/Polymorphism-Lab/Shapes/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+
+            Shape rectangle = new Rectangle(4, 10);
+
+            shapes.Add(rectangle);
+
+            Shape circle = new Circle(10);
+
+            shapes.Add(circle);
+
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine(shape.Draw());
+                Console.WriteLine($"{shape.CalculateArea():f2}");
+                Console.WriteLine($"{shape.CalculatePerimeter():f2}");
+            }
+        }
+    }
+}
