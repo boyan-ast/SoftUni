da4b5f7ec2dd54a3bd408ebdb77ebd60af917232
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-03-06 14:57:17 +0200

    Polymorphism - Vehicles problem solved

---
+++ C# OOP/Polymorphism-Exercise/Vehicles/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vehicles
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] carInfo = Console.ReadLine().Split();
+            string[] truckInfo = Console.ReadLine().Split();
+
+            Vehicle car = new Car(double.Parse(carInfo[1]), double.Parse(carInfo[2]));
+            Vehicle truck = new Truck(double.Parse(truckInfo[1]), double.Parse(truckInfo[2]));
+
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Vehicle> vehiclesByType = new Dictionary<string, Vehicle>() 
+            { 
+                { nameof(Car), car }, 
+                { nameof(Truck), truck } 
+            };
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                string action = command[0];
+                string vehicleType = command[1];
+                double amount = double.Parse(command[2]);
+
+                if (action == "Drive")
+                {
+                    Console.WriteLine(vehiclesByType[vehicleType].Drive(amount));
+                }
+                else if (action == "Refuel")
+                {
+                    vehiclesByType[vehicleType].Refuel(amount);
+                }                
+            }
+
+            foreach (var vehicle in vehiclesByType)
+            {
+                Console.WriteLine(vehicle.Value);
+            }
+        }
+    }
+}
---
+++ C# OOP/Polymorphism-Exercise/Vehicles/Vehicle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        public Vehicle(double fuelQuantity, double fuelConsumption)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public double FuelQuantity { get; protected set; }
+
+        public double FuelConsumption { get; protected set; }
+
+        public string Drive(double distance)
+        {
+            bool canDrive = this.FuelQuantity > this.FuelConsumption * distance;
+
+            if (canDrive)
+            {
+                this.FuelQuantity -= this.FuelConsumption * distance;
+
+                return $"{this.GetType().Name} travelled {distance} km";
+            }
+            else
+            {
+                return $"{this.GetType().Name} needs refueling";
+            }
+        }
+
+        public virtual void Refuel(double liters)
+        {
+            this.FuelQuantity += liters;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name}: {this.FuelQuantity:f2}";
+        }
+    }
+}
