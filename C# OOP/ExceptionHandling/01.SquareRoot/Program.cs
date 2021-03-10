85c04181c3d9c9bc7eec95c7c53076fc66e305d4
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-03-10 13:54:44 +0200

    Exception Handling - Lab

---
+++ C# OOP/ExceptionHandling/01.SquareRoot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    class Program
+    {
+        public static double Sqrt(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException("value", "Sqrt for neagative numbers is undefined!");
+            }
+
+            return Math.Sqrt(value);
+        }
+
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(Sqrt(number));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);              
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
---
+++ C# OOP/ExceptionHandling/02.EnterNumber/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _02.EnterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                int number = default(int);
+
+                try
+                {
+                    number = ReadNumber(1, 100);
+                    numbers[i] = number;
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    i--;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < start || number > end)
+            {
+                throw new ArgumentOutOfRangeException($"{number}", "The number is not in the range!");
+            }
+
+
+            return number;
+        }
+    }
+
+
+
+}
