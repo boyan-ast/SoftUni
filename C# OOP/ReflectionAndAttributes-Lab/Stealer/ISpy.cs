8da32ca3dc4f341aaf3a50733a29b07ad26b089c
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-03-16 16:31:32 +0200

    Reflection and Attributes -  Lab, 3 problems solved

---
+++ C# OOP/ReflectionAndAttributes-Lab/Stealer/ISpy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stealer
+{
+    public interface ISpy
+    {
+        string StealFieldInfo(string className, params string[] fieldsNames);
+
+        string AnalyzeAccessModifiers(string className);
+
+        string RevealPrivateMethods(string className);
+    }
+}
---
+++ C# OOP/ReflectionAndAttributes-Lab/Stealer/Spy.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+
+namespace Stealer
+{
+    public class Spy : ISpy
+    {
+        public string AnalyzeAccessModifiers(string className)
+        {
+            Type classType = GetTypeOfClass(className);
+
+            FieldInfo[] classSelectedFields = classType
+                .GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            MethodInfo[] classPublicMethods = classType
+                .GetMethods(BindingFlags.Public | BindingFlags.Instance);
+            MethodInfo[] classNonPublicMethods = classType
+                .GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (FieldInfo field in classSelectedFields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+
+            foreach (MethodInfo privateGetMethod in classNonPublicMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{privateGetMethod.Name} have to be public!");
+            }
+
+            foreach (MethodInfo publicSetMethod in classPublicMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{publicSetMethod.Name} have to be private!");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string RevealPrivateMethods(string className)
+        {
+            Type classType = GetTypeOfClass(className);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"All Private Methods of Class: {classType.FullName}");
+            sb.AppendLine($"Base Class: {classType.BaseType.Name}");
+
+            MethodInfo[] privateMethodsInfo = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            foreach (MethodInfo privateMethodInfo in privateMethodsInfo)
+            {
+                sb.AppendLine(privateMethodInfo.Name);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string StealFieldInfo(string className, params string[] fieldsNames)
+        {
+            Type hackerType = GetTypeOfClass(className);
+
+            FieldInfo[] fieldsInfo = hackerType.GetFields(
+                BindingFlags.Public | BindingFlags.NonPublic
+                | BindingFlags.Instance | BindingFlags.Static);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Class under investigation: {hackerType.FullName}");
+
+            object classInstance = Activator.CreateInstance(hackerType, new object[] { });
+
+            foreach (string fieldName in fieldsNames)
+            {
+                var selectedField = fieldsInfo.FirstOrDefault(f => f.Name == fieldName);
+
+                if (selectedField != null)
+                {
+                    sb.AppendLine($"{selectedField.Name} = {selectedField.GetValue(classInstance)}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private Type GetTypeOfClass(string className)
+        {
+            Type type = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .FirstOrDefault(t => t.FullName == className);
+
+            if (type == null)
+            {
+                throw new ArgumentException("Invalid Class Name Provided!");
+            }
+
+            return type;
+        }
+    }
+}
