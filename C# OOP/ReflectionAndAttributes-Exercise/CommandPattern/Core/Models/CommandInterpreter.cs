dbc16b029ae31ba3705b1e31bda59195a2cf8dc1
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-03-18 16:53:25 +0200

    Reflection and Attributes - Exercise problems solved

---
+++ C# OOP/ReflectionAndAttributes-Exercise/CommandPattern/Core/Models/CommandInterpreter.cs
@@ -0,0 +1,34 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core.Models
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string commandPostfix = "Command";
+
+        public string Read(string args)
+        {
+            string[] commandArgs = args.Split();
+
+            string action = commandArgs[0];
+
+            Type commandType = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .Where(t => t.GetInterfaces()
+                       .Any(i => i.Name == nameof(ICommand)))
+                .FirstOrDefault(c => c.Name == (action + commandPostfix));
+
+            string[] clearArgs = commandArgs.Skip(1).ToArray();
+
+            ICommand command = (ICommand)Activator.CreateInstance(commandType);
+
+            string result = command.Execute(clearArgs);
+
+            return result;
+        }
+    }
+}
---
+++ C# OOP/ReflectionAndAttributes-Exercise/ValidationAttributes/Models/Validator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Models
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type type = obj.GetType();
+            PropertyInfo[] allProperties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance | 
+                                                              BindingFlags.Static | BindingFlags.NonPublic);
+            foreach (PropertyInfo property in allProperties)
+            {
+                List<MyValidationAttribute> myAttributes = property.GetCustomAttributes<MyValidationAttribute>().ToList();
+
+                foreach (var attribute in myAttributes)
+                {
+                    object value = property.GetValue(obj);
+
+                    if (!attribute.IsValid(value))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
