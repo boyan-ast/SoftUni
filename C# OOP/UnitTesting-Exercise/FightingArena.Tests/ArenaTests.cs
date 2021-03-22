ddb2f3baf0bfa03747a9f50a726a84d661b808ed
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-03-22 12:49:33 +0200

    Unit Testing - Exercise, last problem solved

--- C# OOP/UnitTesting-Exercise/FightingArena.Tests/ArenaTests.cs
+++ C# OOP/UnitTesting-Exercise/FightingArena.Tests/ArenaTests.cs
@@ -1,18 +1,146 @@
-﻿using NUnit.Framework;
+﻿using FightingArena;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tests
 {
     public class ArenaTests
     {
+        private List<Warrior> warriors = new List<Warrior>();
+        private Arena arena;
+        private Warrior warrior;
+        private Warrior defender;
+
+        private string warriorName = "Attacker";
+        private int warriorDamage = 10;
+        private int warriorHp = 100;
+
+        private string defenderName = "Defender";
+        private int defenderDamage = 20;
+        private int defenderHp = 200;
+
         [SetUp]
         public void Setup()
         {
+            this.arena = new Arena();
+            this.warrior = new Warrior(this.warriorName, this.warriorDamage, this.warriorHp);
+            this.defender = new Warrior(this.defenderName, this.defenderDamage, this.defenderHp);
         }
 
         [Test]
-        public void Test1()
+        public void ConstructoShouldCreateInstanceOfEmptyWarriorlist()
         {
-            Assert.Pass();
+            CollectionAssert.AreEqual(new List<Warrior>(), this.arena.Warriors);
+        }
+
+        [Test]
+        public void ConstroctorShouldCreateInstanceWithZeroCount()
+        {
+            Assert.That(this.arena.Count, Is.EqualTo(0));                
+        }
+
+        [Test]
+        public void WarriorsShouldReturnCorrectCollection()
+        {
+            List<Warrior> group = new List<Warrior>()
+            {
+                new Warrior("Test1", 1, 1),
+                new Warrior("Test2", 1, 1),
+                new Warrior("Test3", 1, 1)
+            };
+
+            foreach (Warrior item in group)
+            {
+                this.arena.Enroll(item);
+            }
+
+            CollectionAssert.AreEqual(group, this.arena.Warriors);
+        }
+
+        [Test]
+        public void EnrollShouldAddWarriorToWarriorsList()
+        {
+            this.arena.Enroll(this.warrior);
+
+            string addedName = this.arena.Warriors.ElementAt(0).Name;
+            int addedDamage = this.arena.Warriors.ElementAt(0).Damage;
+            int addedHp = this.arena.Warriors.ElementAt(0).HP;
+
+            Assert.That(addedName, Is.EqualTo(this.warriorName));
+            Assert.That(addedDamage, Is.EqualTo(this.warriorDamage));
+            Assert.That(addedHp, Is.EqualTo(this.warriorHp));
+        }
+
+        [Test]
+        public void AddingExistingWarriorNameShouldThrow()
+        {
+            this.arena.Enroll(this.warrior);
+            Warrior newWarrior = new Warrior(this.warriorName, 150, 1500);
+
+            Assert.That(() => { this.arena.Enroll(newWarrior); },
+                Throws.InvalidOperationException.With.Message
+                .EqualTo("Warrior is already enrolled for the fights!"));
+        }
+
+        [Test]
+        public void EnrollingShouldIncreaseCountWithOne()
+        {
+            this.arena.Enroll(this.warrior);
+
+            Assert.That(this.arena.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CountShouldReturnTheWarriorsNumber()
+        {
+            Warrior secondWarrior = new Warrior("Second", 200, 2000);
+
+            arena.Enroll(this.warrior);
+            arena.Enroll(secondWarrior);
+
+            Assert.That(this.arena.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void WhenFightingNotExistingAttackerShouldThrow()
+        {
+            this.arena.Enroll(this.defender);
+
+            Assert.That(() => { this.arena.Fight(this.warriorName, this.defenderName); },
+                Throws.InvalidOperationException.With.Message
+                .EqualTo($"There is no fighter with name {this.warriorName} enrolled for the fights!"));
+        }
+
+        [Test]
+        public void WhenFightingNoExistingDefenderShouldThrow()
+        {
+            this.arena.Enroll(this.warrior);
+
+            Assert.That(() => { this.arena.Fight(this.warriorName, this.defenderName); },
+                Throws.InvalidOperationException.With.Message
+                .EqualTo($"There is no fighter with name {this.defenderName} enrolled for the fights!"));
+        }
+
+        [Test]
+        public void WhenFightingTwoNotExistngWarriorsShouldThrow()
+        {
+            Assert.That(() => { this.arena.Fight(this.warriorName, this.defenderName); },
+                Throws.InvalidOperationException.With.Message
+                .EqualTo($"There is no fighter with name {this.defenderName} enrolled for the fights!"));
+        }
+        
+        [Test]
+        public void WhenFightingWarriorShouldAttackDefender()
+        {
+            this.arena.Enroll(warrior);
+            this.arena.Enroll(defender);
+
+            this.arena.Fight(this.warriorName, this.defenderName);
+
+            Assert.That(this.warrior.HP, Is.EqualTo(this.warriorHp - this.defenderDamage));
+            Assert.That(this.defender.HP, Is.EqualTo(this.defenderHp - this.warrior.Damage));
+
         }
     }
 }
--- C# OOP/UnitTesting-Exercise/FightingArena.Tests/WarriorTests.cs
+++ C# OOP/UnitTesting-Exercise/FightingArena.Tests/WarriorTests.cs
@@ -1,18 +1,133 @@
+using FightingArena;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class WarriorTests
     {
+        private const int MinAttackHp = 30; 
+
+        private string name = "Test";
+        private int damage = 10;
+        private int hp = 100;
+        private Warrior warrior;
+        private Warrior enemy;
+        private string enemyName = "TestAttacked";
+        private int enemyDamage = 20;
+        private int enemyHp = 200;
+
         [SetUp]
         public void Setup()
         {
+            this.warrior = new Warrior(this.name, this.damage, this.hp);
+            this.enemy = new Warrior(this.enemyName, this.enemyDamage, this.enemyHp);
         }
 
         [Test]
-        public void Test1()
+        public void ConstructorShouldCreateWarriorWithCorrectName()
         {
-            Assert.Pass();
+            Assert.That(this.warrior.Name, Is.EqualTo(this.name));
+        }
+
+        [Test]
+        public void ConstructorShouldCreateWarriorWithCorrectDamage()
+        {
+            Assert.That(this.warrior.Damage, Is.EqualTo(this.damage));
+        }
+
+        [Test]
+        public void ConstructorShouldCreateWarriorWithCorrectHp()
+        {
+            Assert.That(this.warrior.HP, Is.EqualTo(this.hp));
+        }
+
+        [Test]
+        public void SettingNullOrWhiteSpaceNameShouldThrow()
+        {
+
+            Assert.That(() => { this.warrior = new Warrior(" ", this.damage, this.hp); },
+                Throws.ArgumentException.With.Message
+                .EqualTo("Name should not be empty or whitespace!"));
+        }
+
+        [Test]
+        public void SettingNegativeDamageShouldThrow()
+        {
+            Assert.That(() => { this.warrior = new Warrior(this.name, -10, this.hp); },
+                Throws.ArgumentException.With.Message
+                .EqualTo("Damage value should be positive!"));
+        }
+
+        [Test]
+        public void SettingZeroDamageShouldThrow()
+        {
+            Assert.That(() => { this.warrior = new Warrior(this.name, 0, this.hp); },
+                Throws.ArgumentException.With.Message
+                .EqualTo("Damage value should be positive!"));
+        }
+
+        [Test]
+        public void SettingNegativeHpShouldThrow()
+        {
+            Assert.That(() => { this.warrior = new Warrior(this.name, this.damage, -100); },
+                Throws.ArgumentException.With.Message
+                .EqualTo("HP should not be negative!"));
+        }
+
+        [Test]
+        public void AttackingWithEqualOrLowerThanMinHpShouldThrow()
+        {
+            this.warrior = new Warrior(this.name, this.damage, 20);
+
+            Assert.That(() => { this.warrior.Attack(this.enemy); },
+                Throws.InvalidOperationException.With.Message
+                .EqualTo("Your HP is too low in order to attack other warriors!"));
+        }
+
+        [Test]
+        public void AttackingEnemyWithEqualOrLowerThanMinHpShouldThrow()
+        {
+            this.enemy = new Warrior(this.enemyName, this.enemyDamage, 25);
+
+            Assert.That(() => { this.warrior.Attack(this.enemy); },
+                Throws.InvalidOperationException.With.Message
+                .EqualTo($"Enemy HP must be greater than {MinAttackHp} in order to attack him!"));
+        }
+
+        [Test]
+        public void AttackingWithHpLowerThanEnemyDamageShouldThrow()
+        {
+            this.enemy = new Warrior(this.enemyName, 120, this.enemyHp);
+
+            Assert.That(() => { this.warrior.Attack(this.enemy); },
+                Throws.InvalidOperationException.With.Message
+                .EqualTo("You are trying to attack too strong enemy"));
+        }
+
+        [Test]
+        public void AttackingEnemyShouldDecreaseHpCorrectly()
+        {
+            this.warrior.Attack(this.enemy);
+
+            Assert.That(this.warrior.HP, Is.EqualTo(this.hp - this.enemyDamage));
+        }
+
+        [Test]
+        public void AttackigWeakerEnemyShouldKillHim()
+        {
+            this.warrior = new Warrior(this.name, 300, this.hp);
+
+            this.warrior.Attack(this.enemy);
+
+            Assert.That(this.enemy.HP, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AttacingEnemyShouldDecreaseHisHpCorrectly()
+        {
+            this.warrior.Attack(this.enemy);
+
+            Assert.That(this.enemy.HP, Is.EqualTo(this.enemyHp - this.damage));
         }
     }
 }
