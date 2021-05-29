69e8d4b7006833f59d2116c8d2db16d6e9bcea86
boyan-ast <boyan.m.ast@gmail.com>
2021-05-29 17:53:54 +0300

    JS Advanced - DOM Intro, Search in List problem solved

--- JS Advanced/04.DOMIntroduction-Exercise/04. Search in List/search.js
+++ JS Advanced/04.DOMIntroduction-Exercise/04. Search in List/search.js
@@ -1,3 +1,15 @@
 function search() {
-   // TODO
+   const towns = Array.from(document.querySelectorAll('ul#towns>li'));
+   const selectedTown = document.querySelector('#searchText').value;
+
+   let count = 0;
+
+   for (let town of towns) {
+      if (town.textContent.includes(selectedTown)) {
+         town.innerHTML = `<bold><u>${town.textContent}</u></bold>`;
+         count++;
+      }
+   }
+
+   document.getElementById('result').textContent = `${count} matches found`;
 }
