24c13c64955ab4de4125ae5b3f5f747d0837dc16
boyan-ast <boyan.m.ast@gmail.com>
2021-05-30 18:05:10 +0300

    JS Advanced - DOM Intro, one exercise problem solved

--- JS Advanced/04.DOMIntroduction-Exercise/05. Table - Search Engine/solution.js
+++ JS Advanced/04.DOMIntroduction-Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,18 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   const rows = document.querySelectorAll('tbody tr');
+   const button = document.querySelector('#searchField');
 
+   function onClick() {
+      const searchText = button.value.toLowerCase();
+
+      for (let row of rows) {
+         if (row.textContent.toLowerCase().includes(searchText)) {
+            row.setAttribute('class', 'select');
+         } else {
+            row.removeAttribute('class');
+         }
+      }
    }
 }
