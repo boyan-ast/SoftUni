da15380d736006f2c8ac54e2f3c8ea959d9bb9ec
boyan-ast <boyan.m.ast@gmail.com>
2021-05-31 16:27:35 +0300

    JS Advanced - DOM Intro, one exercise problem solved

--- JS Advanced/04.DOMIntroduction-Exercise/06. Format the Text/solution.js
+++ JS Advanced/04.DOMIntroduction-Exercise/06. Format the Text/solution.js
@@ -1,3 +1,32 @@
 function solve() {
-  //TODO
+  const input = document.querySelector('#input').value;
+
+  const textArr = input.split('.').filter(x => x.length > 0);
+
+  const groups = Math.floor(textArr.length / 3);
+
+  //console.log(groups);
+
+  const groupedText = [];
+
+  for (let i = 0; i < groups; i++) {
+    groupedText[i] = [];
+    for (let j = 0; j < 3; j++) {
+      groupedText[i].push(textArr.shift());
+    }
+  }
+
+  if (textArr.length) {
+    groupedText.push(textArr);
+  }
+
+  //console.log(groupedText);
+
+  let result = '';
+
+  for (let i = 0; i < groupedText.length; i++) {
+    result += `<p>${groupedText[i].join('.')}.</p>\n`;
+
+    document.querySelector('#output').innerHTML = result;
+  }
 }
