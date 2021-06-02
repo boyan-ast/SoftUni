79be29372c6870c31372136d686dcc53ad6aa6bf
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-06-02 15:48:48 +0300

    JS Advanced - DOM Intro, 2 exercise problems solved

--- JS Advanced/04.DOMIntroduction-Exercise/08. Generate Report/report.js
+++ JS Advanced/04.DOMIntroduction-Exercise/08. Generate Report/report.js
@@ -1,3 +1,23 @@
 function generateReport() {
-    //TODO
+    let checkboxElements = document.querySelectorAll('input[type="checkbox"]');
+    let checkboxes = Array.from(checkboxElements);
+
+    let dataElements = document.querySelectorAll('tbody tr');
+
+    const selectedObjects = [];
+
+    for (let row of dataElements) {
+
+        let obj = {};
+
+        for (let i = 0; i< row.children.length; i++) {
+            if(checkboxes[i].checked) {
+                obj[checkboxes[i].name] = row.children[i].textContent;
+            }
+        }
+
+        selectedObjects.push(obj);
+    }
+
+    document.getElementById('output').textContent = JSON.stringify(selectedObjects);
 }
--- JS Advanced/04.DOMIntroduction-Exercise/09. Number Convertor/solution.js
+++ JS Advanced/04.DOMIntroduction-Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    const selectMenuElement = document.getElementById('selectMenuTo');
+    const binaryOptionElement = document.createElement('option');
+    const hexadecimalOptionElement = document.createElement('option');
 
-    //TODO...
+    binaryOptionElement.textContent = 'Binary';
+    binaryOptionElement.value = 'binary';
+
+    hexadecimalOptionElement.textContent = 'Hexadecimal';
+    hexadecimalOptionElement.value = 'hexadecimal';
+
+    selectMenuElement.appendChild(binaryOptionElement);
+    selectMenuElement.appendChild(hexadecimalOptionElement);
+
+    const selectMap = {
+        'binary': num => num.toString(2),
+        'hexadecimal': num => num.toString(16).toUpperCase()  
+    }
+
+    const convertButton = document.querySelector('#container>button');
+
+    convertButton.addEventListener('click', (event) => {
+        const inputDataElement = document.getElementById('input');
+        const outputDataElement = document.getElementById('result');
+
+        outputDataElement.value = selectMap[selectMenuElement.value](Number(inputDataElement.value));
+    });
 }
