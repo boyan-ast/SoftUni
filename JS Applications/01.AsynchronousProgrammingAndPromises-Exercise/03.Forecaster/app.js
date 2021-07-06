d6a762b7d2161642aba1d67895ec679cdbc7a10c
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-07-06 13:19:34 +0300

    02.Forecaster problem solved

--- JS Applications/01.AsynchronousProgrammingAndPromises-Exercise/03.Forecaster/app.js
+++ JS Applications/01.AsynchronousProgrammingAndPromises-Exercise/03.Forecaster/app.js
@@ -4,11 +4,19 @@ let divForecastElement = document.getElementById('forecast');
 let divCurrentElement = document.getElementById('current');
 let divUpcomingElement = document.getElementById('upcoming');
 
-function attachEvents() {
-    submitButtonElement.addEventListener('click', getWeather);    
+let weatherSymbols = {
+    Sunny: '☀',
+    'Partly sunny': '⛅',
+    Overcast: '☁',
+    Rain: '☂',
 }
 
-function getWeather(e){
+function attachEvents() {
+    submitButtonElement.addEventListener('click', getWeather);
+}
+
+function getWeather() {
+    divForecastElement.style.display = 'block';
     let locationsUrl = 'http://localhost:3030/jsonstore/forecaster/locations';
 
     fetch(locationsUrl)
@@ -16,11 +24,9 @@ function getWeather(e){
         .then(locationsInfo => {
             let location = locationInputElement.value;
             locationInputElement.value = '';
+
             let cityInfo = locationsInfo.find(c => c.name == location);
-            console.log(cityInfo);
             let cityCode = cityInfo.code;
-            let cityName = cityInfo.name;
-            console.log(cityCode, cityName);
 
             let cityForecastUrl = `http://localhost:3030/jsonstore/forecaster/today/${cityCode}`;
             let cityUpcomingForecastUrl = `http://localhost:3030/jsonstore/forecaster/upcoming/${cityCode}`;
@@ -28,20 +34,101 @@ function getWeather(e){
             fetch(cityForecastUrl)
                 .then(response => response.json())
                 .then(forecastInfo => {
+                    Array.from(divCurrentElement.children)
+                        .slice(1)
+                        .forEach(el => el.remove());
+
                     let city = forecastInfo.name;
                     let condition = forecastInfo.forecast.condition;
                     let high = forecastInfo.forecast.high;
                     let low = forecastInfo.forecast.low;
 
-                    console.log(city, condition, high, low);
+                    let divForecastsElement = document.createElement('div');
+                    divForecastsElement.classList.add('forecasts');
+
+                    let spanSymbolElement = document.createElement('span');
+                    spanSymbolElement.classList.add('condition', 'symbol');
+                    spanSymbolElement.textContent = weatherSymbols[condition];
+
+                    let spanWeatherElement = document.createElement('span');
+                    spanWeatherElement.classList.add('condition');
+
+                    let spanCityElement = document.createElement('span');
+                    spanCityElement.classList.add('forecast-data');
+                    spanCityElement.textContent = city;
+
+                    let spanTempElement = document.createElement('span');
+                    spanTempElement.classList.add('forecast-data');
+                    spanTempElement.textContent = `${low}°/${high}°`;
+
+                    let spanConditionElement = document.createElement('span');
+                    spanConditionElement.classList.add('forecast-data');
+                    spanConditionElement.textContent = condition;
+
+                    spanWeatherElement.appendChild(spanCityElement);
+                    spanWeatherElement.appendChild(spanTempElement);
+                    spanWeatherElement.appendChild(spanConditionElement);
+
+                    divForecastsElement.appendChild(spanSymbolElement);
+                    divForecastsElement.appendChild(spanWeatherElement);
+
+                    divCurrentElement.appendChild(divForecastsElement);
                 });
 
             fetch(cityUpcomingForecastUrl)
                 .then(response => response.json())
                 .then(upcomingInfo => {
-                    let city = upcomingInfo.name;
+                    Array.from(divUpcomingElement.children)
+                        .slice(1)
+                        .forEach(el => el.remove());
+
                     let forecastArray = upcomingInfo.forecast;
+
+                    let divForecastInfo = document.createElement('div');
+                    divForecastInfo.classList.add('forecast-info');
+
+                    forecastArray.forEach(dayInfo => {
+                        let condition = dayInfo.condition;
+                        let high = dayInfo.high;
+                        let low = dayInfo.low;
+
+                        let spanUpcomingElement = document.createElement('span');
+                        spanUpcomingElement.classList.add('upcoming');
+
+                        let spanSymbolElement = document.createElement('span');
+                        spanSymbolElement.classList.add('symbol');
+                        spanSymbolElement.textContent = weatherSymbols[condition];
+
+                        let spanTempElement = document.createElement('span');
+                        spanTempElement.classList.add('forecast-data');
+                        spanTempElement.textContent = `${low}°/${high}°`;
+
+                        let spanConditionElement = document.createElement('span');
+                        spanConditionElement.classList.add('forecast-data');
+                        spanConditionElement.textContent = condition;
+
+                        spanUpcomingElement.appendChild(spanSymbolElement);
+                        spanUpcomingElement.appendChild(spanTempElement);
+                        spanUpcomingElement.appendChild(spanConditionElement);
+
+                        divForecastInfo.appendChild(spanUpcomingElement);
+                    });
+
+                    divUpcomingElement.appendChild(divForecastInfo);
                 });
+        })
+        .catch(() => {
+            Array.from(divCurrentElement.children)
+                .slice(1)
+                .forEach(el => el.remove());
+            Array.from(divUpcomingElement.children)
+                .slice(1)
+                .forEach(el => el.remove());
+
+            let divForecastsElement = document.createElement('div');
+            divForecastsElement.classList.add('forecasts');
+            divForecastsElement.textContent = 'Error';
+            divCurrentElement.appendChild(divForecastsElement);
         });
 }
 
