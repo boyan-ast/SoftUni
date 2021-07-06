0c079b76803ae7f406f04721c4a32333a87e6ef4
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-07-06 11:03:28 +0300

    02.BusSchedule problem solved

--- JS Applications/01.AsynchronousProgrammingAndPromises-Exercise/02.Bus-Schedule/app.js
+++ JS Applications/01.AsynchronousProgrammingAndPromises-Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,34 @@
 function solve() {
+    let spanInfoElement = document.querySelector('#info .info');
+    let departButtonElement = document.getElementById('depart');
+    let arriveButtonElement = document.getElementById('arrive');
+
+    let nextStopId = 'depot';
+    let currentStopId = 'depot';
 
     function depart() {
-        console.log('Depart TODO...');
-    }
+        let stopIdUrl = `http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`;
 
+        fetch(stopIdUrl)
+            .then(body => body.json())
+            .then(stopInfo => {
+                spanInfoElement.textContent = `Next stop ${stopInfo.name}`;
+                nextStopId = stopInfo.next;
+                departButtonElement.disabled = true;
+                arriveButtonElement.disabled = false;
+            });
+    }
     function arrive() {
-        console.log('Arrive TODO...');
+        let stopIdUrl = `http://localhost:3030/jsonstore/bus/schedule/${currentStopId}`;
+
+        fetch(stopIdUrl)
+            .then(body => body.json())
+            .then(stopInfo => {
+                spanInfoElement.textContent = `Arriving at ${stopInfo.name}`;
+                currentStopId = nextStopId;
+                departButtonElement.disabled = false;
+                arriveButtonElement.disabled = true;
+            });
     }
 
     return {
