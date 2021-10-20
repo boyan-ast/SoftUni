90dd6fcbc05f83688749ea69f9d9663a1983b564
boyan-ast <62556633+boyan-ast@users.noreply.github.com>
2021-10-20 15:22:46 +0300

    05.ChangeTownNamesCasing problem solved
    ADO.NET

---
+++ Entity Framework Core/01.ADO.NET-Exercise/ADO.NET-Exercise/05.ChangeTownNamesCasing/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace _05.ChangeTownNamesCasing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+
+            SqlConnection dbConnection = new SqlConnection("Server=.\\SQLEXPRESS;Database=MinionsDB;Integrated Security=true");
+
+            dbConnection.Open();
+
+            using (dbConnection)
+            {
+                SqlCommand updateTownNames = new SqlCommand(Queries.UpdateTownsToUppercase, dbConnection);
+                updateTownNames.Parameters.AddWithValue("@countryName", country);
+
+                int rowsAffected = updateTownNames.ExecuteNonQuery();
+
+                if (rowsAffected == 0)
+                {
+                    Console.WriteLine("No town names were affected.");
+                    return;
+                }
+
+                List<string> updatedTowns = new List<string>();
+
+                SqlCommand selectUpdatedTowns = new SqlCommand(Queries.FindTownByCountry, dbConnection);
+                selectUpdatedTowns.Parameters.AddWithValue("@countryName", country);
+
+                SqlDataReader reader = selectUpdatedTowns.ExecuteReader();
+
+                using (reader)
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            updatedTowns.Add((string)reader["Name"]);
+                        }
+
+                        Console.WriteLine($"{rowsAffected} town names were affected.");
+                        Console.Write('[');
+                        Console.Write(string.Join(", ", updatedTowns));
+                        Console.WriteLine(']');
+                    }
+                    else
+                    {
+                        Console.WriteLine("No town names were affected.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Entity Framework Core/01.ADO.NET-Exercise/ADO.NET-Exercise/05.ChangeTownNamesCasing/Queries.cs
@@ -0,0 +1,16 @@
+﻿namespace _05.ChangeTownNamesCasing
+{
+    static class Queries
+    {
+        public const string FindTownByCountry = @" 
+                         SELECT t.Name 
+                           FROM Towns as t
+                           JOIN Countries AS c ON c.Id = t.CountryCode
+                          WHERE c.Name = @countryName";
+
+        public const string UpdateTownsToUppercase = @"
+                        UPDATE Towns
+                           SET Name = UPPER(Name)
+                         WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
+    }
+}
