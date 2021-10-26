05fe3ab5e88990d3f75712d4972c5bca8045c183
boyan-ast <62556633+boyan-ast@users.noreply.github.com>
2021-10-26 13:36:11 +0300

    EF Introduction, 4 exercise problems solved
    EF Core

---
+++ Entity Framework Core/03.EFIntroduction-Exercise/EFIntroduction-Exercise/SoftUni/StartUp.cs
@@ -0,0 +1,65 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(new SoftUniContext()));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var employees = context.Employees.Select(x => new { x.FirstName, x.LastName, x.MiddleName, x.JobTitle, x.Salary }).ToList();
+
+            foreach (var e in employees)
+            {
+                result.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return result.ToString().Trim();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName);
+
+            foreach (Employee employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(x => x.Department.Name == "Research and Development")
+                .OrderBy(x => x.Salary)
+                .ThenByDescending(x => x.FirstName)
+                .Select(x => new { FullName = x.FirstName + " " + x.LastName, Department = x.Department.Name, Salary = x.Salary })
+                .ToList();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FullName} from {e.Department} - ${e.Salary:f2}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
