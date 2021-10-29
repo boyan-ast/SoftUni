2fdd8e387628522e9be9e3719e5b3774a35f7a4e
boyan-ast <62556633+boyan-ast@users.noreply.github.com>
2021-10-29 14:19:49 +0300

    Entity Relations - Student System problem solved
    EF Core

---
+++ Entity Framework Core/04.EntityRelations-Exercise/EntityRelations/P01_StudentSystem/Data/Models/Homework.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Homework
+    {
+        public int HomeworkId { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public ContentType ContentType { get; set; }
+
+        public DateTime SubmissionTime { get; set; }
+
+        [Required]
+        public int StudentId { get; set; }
+
+        public Student Student { get; set; }
+
+        [Required]
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
---
+++ Entity Framework Core/04.EntityRelations-Exercise/EntityRelations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=StudentSystem;Integrated Security=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .Property(x => x.PhoneNumber)
+                .HasColumnType("char(10)");
+
+            modelBuilder.Entity<Resource>()
+                .Property(x => x.Url)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Resource>()
+                .Property(x => x.ResourceType)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<Homework>()
+                .Property(x => x.Content)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Homework>()
+                .Property(x => x.ContentType)
+                .HasConversion<string>();
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(x => new { x.StudentId, x.CourseId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
