3dcdefbd33cfd15a5f689aeb586be933b750bfe5
Boyan Astardzhiev <boyan_gb@hotmail.com>
2021-11-25 15:23:20 +0200

    Exam Preparation, first problem of Exam 14082020 solved
    EF Core

---
+++ Entity Framework Core/Exam Preparation/Exam14082020/SoftJail/DataProcessor/Deserializer.cs
@@ -0,0 +1,35 @@
+﻿namespace SoftJail.DataProcessor
+{
+
+    using Data;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Deserializer
+    {
+        public static string ImportDepartmentsCells(SoftJailDbContext context, string jsonString)
+        {
+            return "TODO";
+        }
+
+        public static string ImportPrisonersMails(SoftJailDbContext context, string jsonString)
+        {
+            return "TODO";
+        }
+
+        public static string ImportOfficersPrisoners(SoftJailDbContext context, string xmlString)
+        {
+            return "TODO";
+        }
+
+        private static bool IsValid(object obj)
+        {
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(obj);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResult, true);
+            return isValid;
+        }
+    }
+}
