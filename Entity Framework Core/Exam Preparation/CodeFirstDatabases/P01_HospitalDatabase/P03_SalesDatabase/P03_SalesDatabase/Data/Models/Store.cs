37e4e3002f1eef55f0d57f73233640f918877759
boyan-ast <62556633+boyan-ast@users.noreply.github.com>
2021-11-28 15:15:11 +0200

    Code First, 2 problems solved
    EF Core

---
+++ Entity Framework Core/Exam Preparation/CodeFirstDatabases/P01_HospitalDatabase/P03_SalesDatabase/P03_SalesDatabase/Data/Models/Store.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public class Store
+    {
+        public Store()
+        {
+            this.Sales = new HashSet<Sale>();
+        }
+
+        public int StoreId { get; set; }
+
+        [MaxLength(80)]
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
---
+++ Entity Framework Core/Exam Preparation/CodeFirstDatabases/P01_HospitalDatabase/P03_SalesDatabase/P03_SalesDatabase/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data;
+using System;
+
+namespace P03_SalesDatabase
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SalesContext();
+            context.Database.Migrate();
+        }
+    }
+}
